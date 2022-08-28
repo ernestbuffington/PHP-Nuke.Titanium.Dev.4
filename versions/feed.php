bfe1ecd8b2fea3b3e89e9cfbfa5794e9d20df316
Ernest Buffington <ernest.buffington@gmail.com>
2022-08-27 21:39:45 -0400

    Create feed.php

---
+++ versions/feed.php
@@ -0,0 +1,48 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Live Feed from the PHP-Nuke Titanium Dev 4 Website</title>
+</head>
+<body> 
+<strong>Current Build:</strong> v4.0.0b Dev 4<hr>
+<strong>PHP-Nuke Titanium Dev 4 Repsoitory</strong> (Nightly Builds)<br />
+This is the Current US Version of PHP-Nuke Titanium v4.0.0b (Dev 4)<br /> 
+This is a hardened version of PHP-Nuke Xtreme and is secure and safe.<br />
+<a href="https://github.com/ernestbuffington/PHP-Nuke.Titanium.Dev.4" target="_blank">https://github.com/ernestbuffington/PHP-Nuke.Titanium.Dev.4</a>
+<hr>
+<strong><i class="bi bi-calendar3"></i> Aug 27th 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> Force SCeditor in Private_Messages<br />
+<strong><i class="bi bi-calendar3"></i> Aug 27th 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> Added Network nconfig.cfg file checking<br />
+<strong><i class="bi bi-calendar3"></i> Aug 27th 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> Added fbconfig.cfg file checking<br />
+<hr>
+<strong><i class="bi bi-calendar3"></i> Aug 24th 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> Fixed Gender Icon Display<br />
+<hr>
+<strong><i class="bi bi-calendar3"></i> Aug 23rd 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> Fixed Network SQL Errors<br />
+<strong><i class="bi bi-calendar3"></i> Aug 23rd 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> Updated Titanium Intall to v1.3<br />
+<strong><i class="bi bi-calendar3"></i> Aug 23rd 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> New Members List Layout<br />
+<strong><i class="bi bi-calendar3"></i> Aug 23rd 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> New Groups List Layout<br />
+<strong><i class="bi bi-calendar3"></i> Aug 23rd 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> New Ghost Mode Feature<br />
+<strong><i class="bi bi-calendar3"></i> Aug 23rd 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> Updated Titanium Core Theme to v1.3<br />
+<strong><i class="bi bi-calendar3"></i> Aug 23rd 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> Removed Google Titanium SDK v5<br />
+<strong><i class="bi bi-calendar3"></i> Aug 23rd 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> CKEditor 4 Group Support<br />
+<strong><i class="bi bi-calendar3"></i> Aug 23rd 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> Fixed New Vistor Log Center Block v1.0<br />
+<strong><i class="bi bi-calendar3"></i> Aug 23rd 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> Updated Version Checker to New Dev 4 website<br />
+<strong><i class="bi bi-calendar3"></i> Aug 23rd 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> New Cemetery Module v1.0<br />
+<strong><i class="bi bi-calendar3"></i> Aug 23rd 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> New Loan Calulator Module v1.0<br />
+<strong><i class="bi bi-calendar3"></i> Aug 23rd 2022 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> New Proof of God Module v1.0<br />
+<hr>
+<strong><i class="bi bi-calendar3"></i> May 5th 2021 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> New Web Links Module Update v1.0.2<br />
+<strong><i class="bi bi-calendar3"></i> May 1st 2021 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> New Facebook Chat Block v1.0<br />
+<hr />
+<strong><i class="bi bi-calendar3"></i> Apr 27th 2021 </strong>&nbsp;<i class="bi bi-info-square"></i> New Google Site Map Generator Module v1.0<br />
+<strong><i class="bi bi-calendar3"></i> Apr 27th 2021 </strong>&nbsp;<i class="bi bi-info-square"></i> New Visitor Log Center Block v1.0<br />
+<strong><i class="bi bi-calendar3"></i> Apr 24th 2021 </strong>&nbsp;<i class="bi bi-info-square"></i> New Blog Signature Mod Block v1.0<br />
+<hr />
+<strong><i class="bi bi-calendar3"></i> Feb 28th 2021 </strong>&nbsp;<i class="bi bi-info-square"></i> New Link Us Module Update v1.0<br />
+<hr />
+<strong><i class="bi bi-calendar3"></i> Jan 5th 2021 </strong>&nbsp;&nbsp;&nbsp;<i class="bi bi-info-square"></i> New Network Module v1.0 (legal documents)<br />
+<strong><i class="bi bi-calendar3"></i> Jan 4th 2021 </strong>&nbsp;&nbsp;&nbsp;<i class="bi bi-info-square"></i> New Shout Box Module and Block Update v1.0<br />
+<strong><i class="bi bi-calendar3"></i> Jan 2nd 2021 </strong>&nbsp;&nbsp;<i class="bi bi-info-square"></i> New Network Projects Module v11.11.11<br />
+<strong><i class="bi bi-calendar3"></i> Jan 1st 2021 </strong>&nbsp;&nbsp;&nbsp;<i class="bi bi-info-square"></i> New Installer updated v1.0 (New US Version)
+</body>
+</html>
