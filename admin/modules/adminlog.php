ae2c13ed690ca8b6eebd432a9a75376467b588fc
Ernest Buffington <ernest.buffington@gmail.com>
2022-08-29 00:26:57 -0400

    Update adminlog.php

--- admin/modules/adminlog.php
+++ admin/modules/adminlog.php
@@ -3,7 +3,6 @@
   PHP-Nuke Titanium | Nuke-Evolution Xtreme : PHP-Nuke Web Portal System
  =======================================================================*/
 
-
 /************************************************************************
    Nuke-Evolution: Advanced Content Management System
    ============================================
@@ -36,34 +35,6 @@ $log = ($_GET['log']) ? $_GET['log'] : die("Invalid Operation");
 
 if (is_mod_admin()) 
 {
-    // @include_once(NUKE_ADMIN_DIR.'language/custom/lang-'.$currentlang.'.php');
-
-    // function view_log($file) 
-    // {
-    //     global $admin_file;
-    //     echo "<strong><center><font size='3'>"._ADMIN_LOG."</font></strong></center><br /><br />";
-    //     $filename = NUKE_INCLUDE_DIR."log/" . $file . ".log";
-    //     if(!is_file($filename)) {
-    //         echo "<center><strong><span style='color:red'>"._ADMIN_LOG_ERRFND."</span></strong></center>";
-    //         return;
-    //     }
-    //     if(filesize($filename) == 0) {
-    //         echo "<strong>"._TRACKER_HEAD_DATE."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"._TRACKER_HEAD_TIME."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"._TRACKER_HEAD_IP."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"._TRACKER_HEAD_MSG."</strong><br />";
-    //         echo "<br /><br /><strong><a href='".$admin_file.".php'>Back</a></strong>";
-    //         echo "<br /><strong><a href='".$admin_file.".php?op=adminlog_clear&log=" . $file . "'><span style=\"color:red\">"._TRACKER_CLEAR."</span></a></strong>";
-    //         return;
-    //     }
-    //     if($handle = @fopen($filename,"r")) {
-    //         $content = @fread($handle, filesize($filename));
-    //         @fclose($handle);
-    //     }
-    //     $content = nl2br($content);
-    //     echo "<strong>"._TRACKER_HEAD_DATE."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"._TRACKER_HEAD_TIME."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"._TRACKER_HEAD_IP."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"._TRACKER_HEAD_MSG."</strong><br />";
-    //     echo $content;
-    //     echo "<br /><br /><strong><a href='".$admin_file.".php'>Back</a></strong>";
-    //     echo "<br /><strong><a href='".$admin_file.".php?op=adminlog_clear&log=" . $file . "'><span style=\"color:red\">"._TRACKER_CLEAR."</span></a></strong>";
-    // }
-
     function view_log($file) 
     {
         global $admin_file, $admlang;
@@ -124,7 +95,7 @@ if (is_mod_admin())
         echo "".$admlang['logs']['cleared']."<br /><br />";
         $filename = NUKE_INCLUDE_DIR."log/" . $file . ".log";
         if(!is_file($filename)) {
-            echo "<center><strong><span style='color:red'>".$admlang['logs']['not_found']."</span></strong></center>";
+            echo "<div align='center'><strong><span style='color:red'>".$admlang['logs']['not_found']."</span></strong></div>";
         } else {
             if(!$handle = fopen($filename,"w")) {
                 echo $admLang['LOGS']['TRACKER_ERR_OPEN'];
