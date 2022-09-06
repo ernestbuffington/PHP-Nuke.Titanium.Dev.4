7311af0b5f5b795f74f46a76955f19cb9f8aec8f
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-06 00:20:23 -0400

    Update x-bootstrap_4_modal.php

--- modules/Titanium_SandBox/x-bootstrap_4_modal.php
+++ modules/Titanium_SandBox/x-bootstrap_4_modal.php
@@ -5,67 +5,73 @@ if (!defined('MODULE_FILE')) {
 }           
 #################################################[ SandBox Example Source Code ]###########################################################################################
 
-                                                                 ######################################################################################################
-                                                                 # $module_name is defined in the module index.php file and can be found in modules/SandBox/index.php #
-# set the current file name!                                     ########################################################## ###########################################
-$current_file_name = basename(__FILE__, '.php');                                                                          # #
-                                                                                                                          # #
-print '<div align="center"><h2>Loading JQuery and Bootstrap into the current module for editing and testing...</h2></div>';# #
-print '<div align="center"><h1>If you are logged in via FTP the current file location is: /public_html/modules/'.$module_name.'/'.$current_file_name.'.php<h1></div>';
-print '<div align="center">It is always a great idea to leave yourself notes like this, it will help you find the file you need!</div>';
+                                                                            ######################################################################################################
+                                                                            # $module_name is defined in the module index.php file and can be found in modules/SandBox/index.php #
+# set the current file name!                                                ########################################################## ###########################################
+$current_file_name = basename(__FILE__, '.php');                                                                                     # #
+                                                                                                                                     # #
+$page_header  = '<div align="center"><h2>Loading JQuery and Bootstrap into the current module for editing and testing...</h2></div>';# #
+$page_header .= '<div align="center"><h1>If you are logged in via FTP the current file location is: /public_html/modules/'.$module_name.'/'.$current_file_name.'.php<h1></div>';
+$page_header .= '<div align="center">It is always a great idea to leave yourself notes like this, it will help you find the file you need!</div>';
 
-print "<a class=\"sourceview\" href=\"#\" onClick = \"tag=this.nextElementSibling; tag.style.display=tag.style.display == 'none' ? 'block':'none'\"><strong>Click here to show | hide source code</strong></a>";
-print "<div id=\"php_source\" style=\"display:none\">";
+$page_header .= "<a class=\"sourceview\" href=\"#\" onClick = \"tag=this.nextElementSibling; tag.style.display=tag.style.display == 'none' ? 'block':'none'\"><strong>Click here to show | hide source code</strong></a>";
+$page_header .= "<div id=\"php_source\" style=\"display:none\">";
+
+print_r($page_header);
+
 OpenTableCode();
 highlight_file("modules/Titanium_SandBox/x-bootstrap_4_modal.php");
 CloseTableCode();
-print "</div><hr />";
+
+$page_header2 = "</div><hr />";
 #################################################[ SandBox Example Source Code ]###########################################################################################
 # TEST CODE GOES HERE - START
 ###########################################################################################################################################################################
 
 # Load the header tags so the browser stays out of quirk mode OPEN TAG
-print "<head>";
+$page_header2 .= "<head>";
 
 # load the style sheet for this template
-print "<link rel=\"StyleSheet\" href=\"modules/Titanium_SandBox/css/x-bootstrap_4_modal.css\" type=\"text/css\">\n\n\n";
+$page_header2 .= "<link rel=\"StyleSheet\" href=\"modules/Titanium_SandBox/css/x-bootstrap_4_modal.css\" type=\"text/css\">\n\n\n";
 
 # Using jQuery 3.6.1 installed locally on your server:
 # This config assumes you have jquery installed to your dev_includes folder which resides off of the root of your portal
 # /public_html/dev_includes/dev_includes/jquery/jquery.js
-print "<script type=\"text/javascript\" language=\"javascript\" src=\"".HTTPS."dev_includes/jquery/jquery.js\"></script>\n";
+$page_header2 .= "<script type=\"text/javascript\" language=\"javascript\" src=\"".HTTPS."dev_includes/jquery/jquery.js\"></script>\n";
 
 # Using Bootstrap 5.2 installed locally on your server:
 # This config assumes you have bootstrap installed to your dev_includes folder which resides off of the root of your portal
 # /public_html/dev_includes/bootstrap/js/bootstrap.min.js
-print "<script type=\"text/javascript\" language=\"javascript\" src=\"".HTTPS."dev_includes/bootstrap/js/bootstrap.min.js\"></script>\n";
+$page_header2 .= "<script type=\"text/javascript\" language=\"javascript\" src=\"".HTTPS."dev_includes/bootstrap/js/bootstrap.min.js\"></script>\n";
 
 # Load the header tags so the browser stays out of quirk mode CLOSE TAG
-print "</head>";
+$page_header2 .= "</head>";
 
 # Check to see if Jquery is working START
-print "<script type=\"text/javascript\" language=\"javascript\">$(function() { alert('JQuery is loaded and working fine!') })</script>\n";
+$page_header2 .= "<script type=\"text/javascript\" language=\"javascript\">$(function() { alert('JQuery is loaded and working fine!') })</script>\n";
 # Check to see if Jquery is working END
 
+print_r($page_header2);
 
-print "<li>Using jQuery 3.6.1 installed locally on your server:<br />";
-print "<li>This config assumes you have jquery installed to your dev_includes folder which resides off of the root of your portal<br />";
-print "<li>/public_html/dev_includes/jquery/jquery.js<br />";
-print "<hr />";
+$page_body = "<li>Using jQuery 3.6.1 installed locally on your server:<br />";
+$page_body .= "<li>This config assumes you have jquery installed to your dev_includes folder which resides off of the root of your portal<br />";
+$page_body .= "<li>/public_html/dev_includes/jquery/jquery.js<br />";
+$page_body .= "<hr />";
 
-print "<li>Using Bootstrap 5.2 installed locally on your server:<br />";
-print "<li>This config assumes you have bootstrap installed to your dev_includes folder which resides off of the root of your portal<br />";
-print "<li>/public_html/dev_includes/bootstrap/js/bootstrap.min.js<br />";
-print "<br />";
+$page_body .= "<li>Using Bootstrap 5.2 installed locally on your server:<br />";
+$page_body .= "<li>This config assumes you have bootstrap installed to your dev_includes folder which resides off of the root of your portal<br />";
+$page_body .= "<li>/public_html/dev_includes/bootstrap/js/bootstrap.min.js<br />";
+$page_body .= "<br />";
 
 ?>
 
 
 <?
-
+print_r($page_body);
 ###########################################################################################################################################################################			
 # TEST CODE GOES HERE - END
 #################################################[ SandBox Example Source Code ]###########################################################################################
-print "<hr /></fieldset>"; 
+$end_of_page = "<hr /></fieldset>";
+print_r($end_of_page); 
 #################################################[ SandBox Example Source Code ]###########################################################################################
 ?> 
