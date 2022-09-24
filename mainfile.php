9a1dc570525e3e1c174072e2a3cabffb5e4e5a21
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-24 13:24:19 -0400

    Update CMS Version Checking

--- mainfile.php
+++ mainfile.php
@@ -58,9 +58,9 @@ if((defined('NUKE_EVO')) || (defined('NUKE_TITANIUM')))return;
 if (realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME']))exit('Access Denied');
 
 # Define File
-define_once('NUKE_EVO', '4.0.0');
+define_once('NUKE_EVO', '2.0.9');
 # Network Support
-define_once('NUKE_TITANIUM', '4.0.0b');
+define_once('NUKE_TITANIUM', '4.0.2');
 # Network Support
 define_once('TITANIUM_BUILD', '4022');
 define_once('CUR_EVO', 'NUKE_EVO');
