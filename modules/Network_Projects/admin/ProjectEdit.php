53af647e416fdf3a2a4965c7c604d71e85c6f276
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-29 17:01:53 -0400

    Update ProjectEdit.php

--- modules/Network_Projects/admin/ProjectEdit.php
+++ modules/Network_Projects/admin/ProjectEdit.php
@@ -64,7 +64,15 @@ echo "</td></tr>\n";
 
 $repn = $repy = "";
 
-if($project['allowreports'] == 0) { $repn = " selected"; } else { $repy = " selected"; }
+if($project['allowreports'] == 0) 
+{ 
+  $repn = " selected"; 
+} 
+else 
+{ 
+  $repy = " selected"; 
+}
+
 echo "<tr><td bgcolor='$bgcolor2'>"._NETWORK_ACTIVATEREPORTS.":</td>\n";
 echo "<td><select name='allowreports'><option value='0'$repn>"._NETWORK_NO."</option>\n";
 echo "<option value='1'$repy>"._NETWORK_YES."</option></select></td></tr>\n";
@@ -76,79 +84,120 @@ echo "<option value='1'$reqy>"._NETWORK_YES."</option></select></td></tr>\n";
 echo "<tr><td bgcolor='$bgcolor2'>"._NETWORK_PRIORITY.":</td>\n";
 echo "<td><select name='priority_id'>\n";
 $prioritylist = $titanium_db2->sql_query("SELECT `priority_id`, `priority_name` FROM `".$network_prefix."_projects_priorities` ORDER BY `priority_weight`");
-while(list($s_priority_id, $s_priority_name) = $titanium_db2->sql_fetchrow($prioritylist)){
+
+while(list($s_priority_id, $s_priority_name) = $titanium_db2->sql_fetchrow($prioritylist))
+{
   if($s_priority_id == $project['priority_id']){ $sel = "SELECTed"; } else { $sel = ""; }
   echo "<option value='$s_priority_id' $sel>$s_priority_name</option>\n";
 }
+
 echo "</select></td></tr>\n";
 echo "<tr><td bgcolor='$bgcolor2'>"._NETWORK_STATUSPERCENT.":</td>\n";
 echo "<td><input type='text' name='project_percent' size='4' value='".$project['project_percent']."'>% "._NETWORK_STATUSPERCENT_CALCULATE."</td></tr>\n";
 echo "<tr><td bgcolor='$bgcolor2'>"._NETWORK_STATUS.":</td>\n";
 echo "<td><select name='status_id'>\n";
+
 $statuslist = $titanium_db2->sql_query("SELECT `status_id`, `status_name` FROM `".$network_prefix."_projects_status` ORDER BY `status_weight`");
-while(list($s_status_id, $s_status_name) = $titanium_db2->sql_fetchrow($statuslist)){
+
+while(list($s_status_id, $s_status_name) = $titanium_db2->sql_fetchrow($statuslist))
+{
   if($s_status_id == $project['status_id']){ $sel = "SELECTed"; } else { $sel = ""; }
   echo "<option value='$s_status_id' $sel>$s_status_name</option>\n";
 }
 echo "</select></td></tr>\n";
-if($project['date_started'] > 0) {
+
+if($project['date_started'] > 0) 
+{
   $sday = date("j",$project['date_started']);
   $smon = date("n",$project['date_started']);
   $syear = date("Y",$project['date_started']);
-} else {
+} 
+else 
+{
   $sday = "00";
   $smon = "00";
   $syear = "0000";
 }
+
 echo "<tr><td bgcolor='$bgcolor2'>"._NETWORK_STARTDATE.":</td>\n";
 echo "<td><select name='project_start_month'>\n<option value='00'>--</option>\n";
-for($i = 1; $i <= 12; $i++){
+
+for($i = 1; $i <= 12; $i++)
+{
   if($i == $smon){ $sel = "SELECTed"; } else { $sel = ""; }
   echo "<option value='$i' $sel>$i</option>\n";
 }
+
 echo "</select><select name='project_start_day'>\n<option value='00'>--</option>\n";
-for($i = 1; $i <= 31; $i++){
+
+for($i = 1; $i <= 31; $i++)
+{
   if($i == $sday){ $sel = "SELECTed"; } else { $sel = ""; }
   echo "<option value='$i' $sel>$i</option>\n";
 }
+
 echo "</select><input type=text name='project_start_year' value='".$syear."' size=4 maxlength=4></td></tr>\n";
-if($project['date_finished'] > 0) {
+
+if($project['date_finished'] > 0) 
+{
   $fday = date("j",$project['date_finished']);
   $fmon = date("n",$project['date_finished']);
   $fyear = date("Y",$project['date_finished']);
-} else {
+} 
+else 
+{
   $fday = "00";
   $fmon = "00";
   $fyear = "0000";
 }
+
 echo "<tr><td bgcolor='$bgcolor2'>"._NETWORK_FINISHDATE.":</td>\n";
 echo "<td><select name='project_finish_month'>\n<option value='00'>--</option>\n";
-for($i = 1; $i <= 12; $i++){
+
+for($i = 1; $i <= 12; $i++)
+{
   if($i == $fmon){ $sel = "SELECTed"; } else { $sel = ""; }
   echo "<option value='$i' $sel>$i</option>\n";
 }
 echo "</select><select name='project_finish_day'>\n<option value='00'>--</option>\n";
-for($i = 1; $i <= 31; $i++){
-  if($i == $fday){ $sel = "SELECTed"; } else { $sel = ""; }
+
+for($i = 1; $i <= 31; $i++)
+{
+  if($i == $fday)
+  { 
+    $sel = "SELECTed"; 
+  } 
+  else 
+  { 
+    $sel = ""; 
+  }
   echo "<option value='$i' $sel>$i</option>\n";
 }
+
 echo "</select><input type=text name='project_finish_year' value='".$fyear."' size='4' maxlength='4'></td></tr>\n";
 echo "<tr><td bgcolor='$bgcolor2' valign='top'>"._NETWORK_ASSIGNMEMBERS.":</td>\n";
 echo "<td><select name='member_ids[]' size='10' multiple>\n";
+
 $memberlistresult = $titanium_db2->sql_query("SELECT `member_id`, `member_name` FROM `".$network_prefix."_members` ORDER BY `member_name`");
-while(list($member_id, $member_name) = $titanium_db2->sql_fetchrow($memberlistresult)) {
+
+while(list($member_id, $member_name) = $titanium_db2->sql_fetchrow($memberlistresult)) 
+{
   $memberexresult = $titanium_db2->sql_query("SELECT `member_id` FROM `".$network_prefix."_projects_members` WHERE `member_id`='$member_id' AND `project_id`='$project_id'");
   $numrows = $titanium_db2->sql_numrows($memberexresult);
-  if($numrows < 1){
+
+  if($numrows < 1)
+  {
     echo "<option value='$member_id'>$member_name</option>\n";
   }
 }
 echo "</select></td></tr>\n";
+
 echo "<tr><td colspan='2' align='center'><input type='submit' value='"._NETWORK_UPDATEPROJECT."'></td></tr>\n";
 echo "</form>\n";
 echo "</table>\n";
+
 CloseTable();
-//echo "<br />";
+
 OpenTable();
 echo "<table width='100%' border='1' cellspacing='0' cellpadding='2'>";
 echo "<form method='post' action='".$admin_file.".php'>";
@@ -157,10 +206,15 @@ echo "<input type='hidden' name='project_id' VALUE='$project_id'>";
 echo "<tr><td align='left' bgcolor='$bgcolor2' width='100%' colspan='2'><strong>"._NETWORK_PROJECTMEMBERS."</strong></td>";
 echo "<td align='center' bgcolor='$bgcolor2'><strong>"._NETWORK_POSITION."</strong></td>";
 echo "<td align='center' bgcolor='$bgcolor2'><strong>"._NETWORK_DELETE."</strong></td></tr>";
+
 $membersresult = $titanium_db2->sql_query("SELECT `member_id`, `position_id` FROM `".$network_prefix."_projects_members` WHERE `project_id`='$project_id'");
+
 $numrows = $titanium_db2->sql_numrows($membersresult);
-if($numrows > 0){
-  while(list($member_id, $position_id) = $titanium_db2->sql_fetchrow($membersresult)){
+
+if($numrows > 0)
+{
+  while(list($member_id, $position_id) = $titanium_db2->sql_fetchrow($membersresult))
+  {
     $member = pjmember_info($member_id);
     $position = pjmemberposition_info($position_id);
     echo "<tr>";
@@ -168,22 +222,36 @@ if($numrows > 0){
     echo "<td><img src='$pjimage'></td><td width='100%'>".$member['member_name']."</td>";
     echo "<td><input type='hidden' name='member_ids[]' VALUE='$member_id'><select name='position_ids[]'>";
     $positionlistresult = $titanium_db2->sql_query("SELECT `position_id`, `position_name` FROM `".$network_prefix."_members_positions` ORDER BY `position_weight`");
-    while(list($l_position_id, $l_position_name) = $titanium_db2->sql_fetchrow($positionlistresult)) {
-      if($l_position_id == $position_id){ $sel = "SELECTed"; } else { $sel = ""; }
+  
+    while(list($l_position_id, $l_position_name) = $titanium_db2->sql_fetchrow($positionlistresult)) 
+	{
+      if($l_position_id == $position_id)
+	  { 
+	    $sel = "SELECTed"; 
+	  } 
+	  else 
+	  { 
+	    $sel = ""; 
+	  }
       echo "<option value='$l_position_id' $sel>$l_position_name</option>";
     }
-    echo "</select></td>";
+    
+	echo "</select></td>";
     echo "<td align=center><nobr><input name='delete_member_ids[]' type='checkbox' value='$member_id'></td>";
     echo "</tr>";
   }
   echo "<tr><td colspan='4' width='100%' align=right bgcolor='$bgcolor2'><input type='submit' value='"._NETWORK_UPDATE."'>";
   echo "<input type='submit' value='"._NETWORK_DELETE."'></td></tr>";
-} else {
+} 
+else 
+{
   echo "<tr><td colspan='4' width='100%' align=center>"._NETWORK_NOPROJECTMEMBERS."</td></tr>";
 }
 echo "</form></table>";
-CloseTable();
-pj_copy();
-include_once(NUKE_BASE_DIR.'footer.php');
 
+CloseTable();
+
+pj_copy();
+
+include_once(NUKE_BASE_DIR.'footer.php');
 ?>
