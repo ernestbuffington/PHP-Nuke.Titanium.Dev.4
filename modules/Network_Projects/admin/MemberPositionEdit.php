d1f17a09e114571fdf720a699b67e5565d269b74
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-29 16:47:21 -0400

    Update MemberPositionEdit.php

--- modules/Network_Projects/admin/MemberPositionEdit.php
+++ modules/Network_Projects/admin/MemberPositionEdit.php
@@ -2,24 +2,33 @@
 /*=======================================================================
  PHP-Nuke Titanium: Enhanced PHP-Nuke Web Portal System
  =======================================================================*/
+
 /********************************************************/
 /* NukeProject(tm)                                      */
 /* By: NukeScripts Network (webmaster@nukescripts.net)  */
 /* http://nukescripts.86it.us                           */
 /* Copyright (c) 2000-2005 by NukeScripts Network       */
 /********************************************************/
+
 global $titanium_db2;
+
 if(!defined('NETWORK_SUPPORT_ADMIN')) { die("Illegal Access Detected!!!"); }
 
 $pagetitle = _NETWORK_TITLE.' v'.$pj_config['version_number'].' - '._NETWORK_MEMBERS.': '._NETWORK_EDITPOSITION;
 
 $position_id = intval($position_id);
-if($position_id < 1) { header("Location: ".$admin_file.".php?op=MemberPositionList"); }
+
+if($position_id < 1) 
+{ 
+  header("Location: ".$admin_file.".php?op=MemberPositionList"); 
+}
+
 include_once(NUKE_BASE_DIR.'header.php');
 
 $position = pjmemberposition_info($position_id);
+
 pjadmin_menu(_NETWORK_MEMBERS.": "._NETWORK_EDITPOSITION);
-//echo "<br />\n";
+
 OpenTable();
 echo "<table align='center' border='0' cellpadding='2' cellspacing='2'>\n";
 echo "<form method='post' action='".$admin_file.".php'>\n";
@@ -30,8 +39,10 @@ echo "<td><input type='text' name='position_name' size='30' value=\"".$position[
 echo "<tr><td colspan='2' align='center'><input type='submit' value='"._NETWORK_UPDATEPOSITION."'></td></tr>\n";
 echo "</form>\n";
 echo "</table>\n";
-CloseTable();
-pj_copy();
-include_once(NUKE_BASE_DIR.'footer.php');
 
+CloseTable();
+
+pj_copy();
+
+include_once(NUKE_BASE_DIR.'footer.php');
 ?>
