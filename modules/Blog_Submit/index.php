253037c972c89dab73e83eb5125c6f629dec7d77
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-29 03:58:08 -0400

    Update Version Info for Blog Submission module!

--- modules/Blog_Submit/index.php
+++ modules/Blog_Submit/index.php
@@ -9,7 +9,7 @@
 /*                                                                      */
 /* Copyright (c) 2002 by Francisco Burzi                                */
 /* http://phpnuke.org                                                   */
-/*                                                                      */
+/* Version 1.0b                                                         */
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
 /* the Free Software Foundation; either version 2 of the License.       */
