c7cfe241aa729fe2712587171d190afa998aece0
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-29 16:38:45 -0400

    Update ConfigUpdate.php

--- modules/Network_Projects/admin/ConfigUpdate.php
+++ modules/Network_Projects/admin/ConfigUpdate.php
@@ -2,6 +2,7 @@
 /*=======================================================================
  PHP-Nuke Titanium: Enhanced PHP-Nuke Web Portal System
  =======================================================================*/
+
 /********************************************************/
 /* NukeProject(tm)                                      */
 /* By: NukeScripts Network (webmaster@nukescripts.net)  */
@@ -9,11 +10,16 @@
 /* Copyright (c) 2000-2005 by NukeScripts Network       */
 /********************************************************/
 global $titanium_db2;
-if(!defined('NETWORK_SUPPORT_ADMIN')) { die("Illegal Access Detected!!!"); }
-$location = htmlentities($location, ENT_QUOTES);
-pjsave_config('reports_active', $reports_active);
-pjsave_config('requests_active', $requests_active);
-pjsave_config('location', $location);
-header("Location: ".$admin_file.".php?op=Config");
 
+if(!defined('NETWORK_SUPPORT_ADMIN')) { die("Illegal Access Detected!!!"); }
+
+$location = htmlentities($location, ENT_QUOTES);
+
+pjsave_config('reports_active', $reports_active);
+
+pjsave_config('requests_active', $requests_active);
+
+pjsave_config('location', $location);
+
+header("Location: ".$admin_file.".php?op=Config");
 ?>
