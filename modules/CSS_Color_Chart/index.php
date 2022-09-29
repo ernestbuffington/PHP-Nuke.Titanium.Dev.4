74c53f50f996c75d885057c559e6d0b55acf99e2
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-29 19:22:52 -0400

    Update index.php

--- modules/CSS_Color_Chart/index.php
+++ modules/CSS_Color_Chart/index.php
@@ -1,30 +1,20 @@
 <?php
-if (!defined('MODULE_FILE')) {
-   die('You can\'t access this file directly...');
-}
+if (!defined('MODULE_FILE')){die('You can\'t access this file directly...');}
 
-$pagetitle = 'CSS Hex Color Chart';
+$pagetitle = 'CSS Hex Color Chart v5.4';
 
 require_once("mainfile.php");
+
 $titanium_module_name = basename(dirname(__FILE__));
+
 get_lang($titanium_module_name);
+
 include("header.php");
+
 $index = 0;
-    #########################################################################
-    # Table Header Module     Fix Start - by TheGhost   v1.0.0     01/30/2012
-    #########################################################################
-    if(!function_exists('OpenTableModule'))
-    {
-      OpenTable();
-      echo "<center><h4>CSS Hex Color Chart</h4>";
-	}
-	else
-	{
-	   OpenTableModule();
-	}
-    #########################################################################
-    # Table Header Module     Fix End  - by TheGhost   v1.0.0     01/30/2012
-    #########################################################################
+
+OpenTable();
+echo "<div align=\"center\"><h1>CSS Hex Color Chart</h1>";
 echo "<br/><table align=\"center\"  width=\"90%\" border=\"1\" cellpadding=\"2\" cellspacing=\"0\" bordercolorlight=\"#787878\" bordercolordark=\"#787878\" style=\"border-collapse: collapse\" bordercolor=\"#787878\">"
   . "<tr align=\"center\">"
   . "<td bgcolor=\"#eeeeee\"><font color=\"#000000\">EEEEEE</font></td>"
@@ -2049,7 +2039,7 @@ echo "<br/><table align=\"center\"  width=\"90%\" border=\"1\" cellpadding=\"2\"
   . "<td bgcolor=\"#FFCC99\"><font color=\"#000000\">Blush</font></td>"
   . "<td>255 204 153</td>"
   . "<td>#FFCC99</td></tr></table><br><br>"
-  ."<CENTER>CSS Hex Color Chart v1&nbsp;by&nbsp;<a href=\"http://cvs.86it.us\">Ernest Allen Buffington</a>";
+  ."</br>CSS Hex Color Chart v5.4&nbsp;by&nbsp;<a href=\"http://theghost.86it.us\">Ernest Allen Buffington</a></div>";
 CloseTable();
 include("footer.php");
 ?>
