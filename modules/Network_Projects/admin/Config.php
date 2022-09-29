72a090bb1ab3caa58a49b83c4472993e24d6c9cd
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-29 16:38:03 -0400

    Update

--- modules/Network_Projects/admin/Config.php
+++ modules/Network_Projects/admin/Config.php
@@ -2,13 +2,16 @@
 /*=======================================================================
  PHP-Nuke Titanium: Enhanced PHP-Nuke Web Portal System
  =======================================================================*/
+
 /********************************************************/
 /* NukeProject(tm)                                      */
 /* By: NukeScripts Network (webmaster@nukescripts.net)  */
 /* http://nukescripts.86it.us                           */
 /* Copyright (c) 2000-2005 by NukeScripts Network       */
 /********************************************************/
+
 global $titanium_db2;
+
 if(!defined('NETWORK_SUPPORT_ADMIN')) { die("Illegal Access Detected!!!"); }
 
 $pagetitle = _NETWORK_TITLE.' v'.$pj_config['version_number'].' - '._NETWORK_GENCONFIG;
@@ -16,13 +19,16 @@ $pagetitle = _NETWORK_TITLE.' v'.$pj_config['version_number'].' - '._NETWORK_GEN
 include_once(NUKE_BASE_DIR.'header.php');
 
 pjadmin_menu(_NETWORK_GENCONFIG);
-//echo "<br />\n";
+
 OpenTable();
 echo "<table align='center' border='0' cellpadding='2' cellspacing='2'>\n";
 echo "<form method='post' action='".$admin_file.".php'>\n";
 echo "<input type='hidden' name='op' value='ConfigUpdate'>\n";
+
 $modlocation = basename(str_replace("/admin", "", dirname(__FILE__)));
+
 echo "<tr><td bgcolor='$bgcolor2' valign='top'><strong>"._NETWORK_MODLOCATION.":</strong></td><td><select name='location'>\n";
+
 $handle=opendir('modules');
 
 while($file = readdir($handle)) 
@@ -33,21 +39,29 @@ while($file = readdir($handle))
   }
 }
 closedir($handle);
+
 $titanium_modulelist = explode(" ", $titanium_modulelist);
+
 sort($titanium_modulelist);
-for($i=0; $i < sizeof($titanium_modulelist); $i++) {
+
+for($i=0; $i < sizeof($titanium_modulelist); $i++) 
+{
   if($titanium_modulelist[$i]!="") {
     echo "<option value=\"$titanium_modulelist[$i]\" ";
     if($titanium_modulelist[$i] == $pj_config['location']) echo "selected";
     echo ">$titanium_modulelist[$i]\n";
   }
 }
+
 echo "</select><br />"._NETWORK_MODLOCATIONNOTE." $modlocation<br />"._NETWORK_MODLOCATIONNOTE2."</td></tr>\n";
 echo "<tr><td colspan='2' align='center'><input type='submit' value='"._NETWORK_CONFIGUPDATE."'></td></tr>\n";
 echo "</form>\n";
 echo "</table>\n";
+
 CloseTable();
+
 pj_copy();
+
 include_once(NUKE_BASE_DIR.'footer.php');
 
 ?>
