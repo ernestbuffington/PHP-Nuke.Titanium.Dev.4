7281a85c5c8ee124b9d1a241e0df0d9b979d70a8
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-29 16:41:53 -0400

    Update Main.php

--- modules/Network_Projects/admin/Main.php
+++ modules/Network_Projects/admin/Main.php
@@ -2,20 +2,24 @@
 /*=======================================================================
  PHP-Nuke Titanium: Enhanced PHP-Nuke Web Portal System
  =======================================================================*/
+
 /********************************************************/
 /* NukeProject(tm)                                      */
 /* By: NukeScripts Network (webmaster@nukescripts.net)  */
 /* http://nukescripts.86it.us                           */
 /* Copyright (c) 2000-2005 by NukeScripts Network       */
 /********************************************************/
+
 global $titanium_db2;
+
 if(!defined('NETWORK_SUPPORT_ADMIN')) { die("Illegal Access Detected!!!"); }
 
 $pagetitle = _NETWORK_TITLE.' v'.$pj_config['version_number'].' - '._NETWORK_ADMIN;
 
 include_once(NUKE_BASE_DIR.'header.php');
+
 pjadmin_menu(_NETWORK_ADMIN);
 pj_copy();
-include_once(NUKE_BASE_DIR.'footer.php');
 
+include_once(NUKE_BASE_DIR.'footer.php');
 ?>
