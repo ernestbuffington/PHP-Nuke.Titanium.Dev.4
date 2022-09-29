cbe2898b9e43b3f558af43b71b46f826d17c5084
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-29 17:17:11 -0400

    Update ProjectPriorityEdit.php

--- modules/Network_Projects/admin/ProjectPriorityEdit.php
+++ modules/Network_Projects/admin/ProjectPriorityEdit.php
@@ -2,25 +2,35 @@
 /*=======================================================================
  PHP-Nuke Titanium: Enhanced PHP-Nuke Web Portal System
  =======================================================================*/
+
 /********************************************************/
 /* NukeProject(tm)                                      */
 /* By: NukeScripts Network (webmaster@nukescripts.net)  */
 /* http://nukescripts.86it.us                           */
 /* Copyright (c) 2000-2005 by NukeScripts Network       */
 /********************************************************/
+
 global $titanium_db2;
+
 get_lang('Network_Projects');
+
 if(!defined('NETWORK_SUPPORT_ADMIN')) { die("Illegal Access Detected!!!"); }
 
 $pagetitle = _NETWORK_TITLE.' v'.$pj_config['version_number'].' - '._NETWORK_PROJECTS.': '._NETWORK_EDITPRIORITY;
 
 $priority_id = intval($priority_id);
-if($priority_id < 1) { header("Location: ".$admin_file.".php?op=ProjectPriorityList"); }
+
+if($priority_id < 1) 
+{ 
+  header("Location: ".$admin_file.".php?op=ProjectPriorityList"); 
+}
+
 include_once(NUKE_BASE_DIR.'header.php');
 
 $priority = pjprojectpriority_info($priority_id);
+
 pjadmin_menu(_NETWORK_PROJECTS.": "._NETWORK_EDITPRIORITY);
-//echo "<br />\n";
+
 OpenTable();
 echo "<table align='center' border='0' cellpadding='2' cellspacing='2'>\n";
 echo "<form method='post' action='".$admin_file.".php'>\n";
@@ -32,7 +42,8 @@ echo "<tr><td colspan='2' align='center'><input type='submit' value='"._NETWORK_
 echo "</form>\n";
 echo "</table>\n";
 CloseTable();
-pj_copy();
-include_once(NUKE_BASE_DIR.'footer.php');
 
+pj_copy();
+
+include_once(NUKE_BASE_DIR.'footer.php');
 ?>
