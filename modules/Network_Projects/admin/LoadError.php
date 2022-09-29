a820a6062e623de2bd2afc319b30301b2db7793c
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-29 16:41:19 -0400

    Update LoadError.php

--- modules/Network_Projects/admin/LoadError.php
+++ modules/Network_Projects/admin/LoadError.php
@@ -2,18 +2,24 @@
 /*=======================================================================
  PHP-Nuke Titanium: Enhanced PHP-Nuke Web Portal System
  =======================================================================*/
+
 /********************************************************/
 /* NukeProject(tm)                                      */
 /* By: NukeScripts Network (webmaster@nukescripts.net)  */
 /* http://nukescripts.86it.us                           */
 /* Copyright (c) 2000-2005 by NukeScripts Network       */
 /********************************************************/
+
 global $titanium_db2;
+
 if(!defined('NETWORK_SUPPORT_ADMIN')) { die("Illegal File Access Detected!!"); }
+
 $pagetitle = "NukeProject(tm): Error Loading Functions";
+
 include_once(NUKE_BASE_DIR.'header.php');
 
 title($pagetitle);
+
 OpenTable();
 echo "It appears that NukeProject(tm) has not been configured correctly.  The
 most common cause is that you either have an error in the syntax that is
@@ -21,6 +27,6 @@ including includes/nsnbc_func.php from your mainfile.php, or you have not
 added the NukeProject(tm) code to your mainfile.php.  Details for including this
 code are included in the download package in the <strong>Edits_For_Core_Files</strong> directory.";
 CloseTable();
-include_once(NUKE_BASE_DIR.'footer.php');
 
+include_once(NUKE_BASE_DIR.'footer.php');
 ?>
