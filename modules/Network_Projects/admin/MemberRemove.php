f9a52156e881a060ee3840c15ef49f18aa769043
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-29 16:52:35 -0400

    Update MemberRemove.php

--- modules/Network_Projects/admin/MemberRemove.php
+++ modules/Network_Projects/admin/MemberRemove.php
@@ -2,13 +2,16 @@
 /*=======================================================================
  PHP-Nuke Titanium: Enhanced PHP-Nuke Web Portal System
  =======================================================================*/
+
 /********************************************************/
 /* NukeProject(tm)                                      */
 /* By: NukeScripts Network (webmaster@nukescripts.net)  */
 /* http://nukescripts.86it.us                           */
 /* Copyright (c) 2000-2005 by NukeScripts Network       */
 /********************************************************/
+
 global $titanium_db2;
+
 if(!defined('NETWORK_SUPPORT_ADMIN')) { die("Illegal Access Detected!!!"); }
 
 $pagetitle = _NETWORK_TITLE.' v'.$pj_config['version_number'].' - '._NETWORK_MEMBERS.': '._NETWORK_DELETEMEMBER;
@@ -16,8 +19,9 @@ $pagetitle = _NETWORK_TITLE.' v'.$pj_config['version_number'].' - '._NETWORK_MEM
 include_once(NUKE_BASE_DIR.'header.php');
 
 $member = pjmember_info($member_id);
+
 pjadmin_menu(_NETWORK_MEMBERS.": "._NETWORK_DELETEMEMBER);
-//echo "<br />\n";
+
 OpenTable();
 echo "<table align='center' border='0' cellpadding='2' cellspacing='2'>\n";
 echo "<form method='post' action='".$admin_file.".php'>\n";
@@ -27,15 +31,20 @@ echo "<tr><td align='center'><strong>"._NETWORK_SWAPMEMBER."</strong></td></tr>\
 echo "<tr><td align='center'>".$member['member_name']." -> <select name='swap_member_id'>\n";
 echo "<option value='0'>---------</option>\n";
 $memberlist = $titanium_db2->sql_query("SELECT `member_id`, `member_name` FROM `".$network_prefix."_members` WHERE `member_id` != '$member_id' ORDER BY `member_name`");
-while(list($s_member_id, $s_member_name) = $titanium_db2->sql_fetchrow($memberlist)){
+
+while(list($s_member_id, $s_member_name) = $titanium_db2->sql_fetchrow($memberlist))
+{
     echo "<option value='$s_member_id'>$s_member_name</option>\n";
 }
+
 echo "</select></td></tr>\n";
 echo "<tr><td align='center'><input type='submit' value='"._NETWORK_DELETEMEMBER."'></td></tr>\n";
 echo "</form>\n";
 echo "</table>\n";
-CloseTable();
-pj_copy();
-include_once(NUKE_BASE_DIR.'footer.php');
 
+CloseTable();
+
+pj_copy();
+
+include_once(NUKE_BASE_DIR.'footer.php');
 ?>
