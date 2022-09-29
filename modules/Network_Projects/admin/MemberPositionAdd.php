021160491cc781da6609a3ac2901bf5294cb0bd0
Ernest Buffington <ernest.buffington@gmail.com>
2022-09-29 16:45:54 -0400

    Update MemberPositionAdd.php

--- modules/Network_Projects/admin/MemberPositionAdd.php
+++ modules/Network_Projects/admin/MemberPositionAdd.php
@@ -2,13 +2,16 @@
 /*=======================================================================
  PHP-Nuke Titanium: Enhanced PHP-Nuke Web Portal System
  =======================================================================*/
+
 /********************************************************/
 /* NukeProject(tm)                                      */
 /* By: NukeScripts Network (webmaster@nukescripts.net)  */
 /* http://nukescripts.86it.us                           */
 /* Copyright (c) 2000-2005 by NukeScripts Network       */
 /********************************************************/
+
 global $titanium_db2;
+
 if(!defined('NETWORK_SUPPORT_ADMIN')) { die("Illegal Access Detected!!!"); }
 
 $pagetitle = _NETWORK_TITLE.' v'.$pj_config['version_number'].' - '._NETWORK_MEMBERS.': '._NETWORK_POSITIONADD;
@@ -16,7 +19,7 @@ $pagetitle = _NETWORK_TITLE.' v'.$pj_config['version_number'].' - '._NETWORK_MEM
 include_once(NUKE_BASE_DIR.'header.php');
 
 pjadmin_menu(_NETWORK_MEMBERS.": "._NETWORK_POSITIONADD);
-//echo "<br />\n";
+
 OpenTable();
 echo "<table align='center' border='0' cellpadding='2' cellspacing='2'>\n";
 echo "<form method='post' action='".$admin_file.".php'>\n";
@@ -27,7 +30,8 @@ echo "<tr><td colspan='2' align='center'><input type='submit' value='"._NETWORK_
 echo "</form>\n";
 echo "</table>\n";
 CloseTable();
-pj_copy();
-include_once(NUKE_BASE_DIR.'footer.php');
 
+pj_copy();
+
+include_once(NUKE_BASE_DIR.'footer.php');
 ?>
