06429f31ef7f7d6d4368047ab39e07cd48f7dbf6
Ernest Buffington <ernest.buffington@gmail.com>
2022-10-13 10:25:55 -0400

    Update

--- themes/86it-Titanium/includes/plugin_facebook_music_comments.php
+++
@@ -1,9 +0,0 @@
-<?php
-global $domain, $name, $facebook_plugin_width;
-
-?>
-<center>
-<div class="fb-comments" data-href="http://music.86it.us/modules.php?name=<?=$name?>&file=article&sid=<?=$sid?>&mode=0&thold=-1" ref="<?=$domain?>" data-num-posts="5" data-width="<?=$facebook_plugin_width?>">
-</div>
-<?
-?>
--- themes/86it-Titanium/includes/plugin_facebook_music_likes.php
+++
@@ -1,9 +0,0 @@
-<?php
-global $domain, $name, $facebook_plugin_width;
-
-?>
-<center>
-<div class="fb-like" data-href="http://music.86it.us/modules.php?name=<?=$name?>&file=article&sid=<?=$sid?>&mode=0&thold=-1" ref="<?=$domain?>" data-send="false"  width="<?=$facebook_plugin_width?>" show_faces="true" font="verdana"></div>
-</center>
-<?
-?>
