6970343d001763940b3a86f86d22aae27b157967
Ernest Buffington <ernest.buffington@gmail.com>
2022-10-17 07:19:03 -0400

    Update function_themearticle.php

--- themes/XtremeV3b/function_themearticle.php
+++ themes/XtremeV3b/function_themearticle.php
@@ -7,7 +7,6 @@ function themearticle($aid, $informant, $datetime, $modified, $title, $counter, 
     global $digits_color, $digits_txt_color;
 
 	if (!empty($topicimage)): 
-
 		$t_image = (file_exists(xtremev3_images_dir.'topics/'.$topicimage)) 
 		? xtremev3_images_dir.'topics/'.$topicimage : $tipath.$topicimage;
 		$topic_img  = '<td width="25%" align="center" class="extra">';
@@ -15,15 +14,10 @@ function themearticle($aid, $informant, $datetime, $modified, $title, $counter, 
 	else: 
 		$topic_img = '';
 	endif;
-	
 	$notes = (!empty($notes)) ? '<br /><br /><strong>'._NOTE.'</strong> '.$notes : '';
-	
 	$content = '';
-	
 	if ($aid == $informant): 
-	    
 		$content = $thetext.$notes;
-	
 	else: 
 		if ($writes):
 			if (!empty($informant)): 
@@ -37,11 +31,9 @@ function themearticle($aid, $informant, $datetime, $modified, $title, $counter, 
 			    $content .= $thetext.$notes;
        endif;
 	endif;
-
     $posted = _POSTEDON.' '.$datetime.' '._BY.' ';
     $posted .= get_author($aid);
     $reads = '( <span style="color: yellow;">Reads</span>: <span style="color: red;">'.$counter.'</span> )';
-
     print '<table class="otthree"border="0" width="100%" cellspacing="0" cellpadding="0">';
     print '<tr>';
     print '<td background="'.HTTPS.'themes/'.$theme_name.'/tables/OpenTable/topmiddle.png" align="left" width="39" colspan="2">';
@@ -56,7 +48,6 @@ function themearticle($aid, $informant, $datetime, $modified, $title, $counter, 
     print '<td width="15" background="'.HTTPS.'themes/'.$theme_name.'/tables/OpenTable/leftside.png"></td>';
     print '<td width="24"></td>';
     print '<td width="100%">';
-
     print '<div align="left" id="text">'.''.$title.'</div><p>';
     print '<div align="left" id="text">';
 	print ''.$posted.'';
@@ -66,9 +57,8 @@ function themearticle($aid, $informant, $datetime, $modified, $title, $counter, 
     facebook_likes();
     facebook_comments();
     print '<!-- facebook functions END -->'."\n\n\n";	
-
-    print '<div align="right"><img src="themes/'.$theme_name.'/images/invisible_pixel.gif" alt="" width="4" height="20" border="0" /><br />'.$reads.'<img src="themes/'.$theme_name.'/images/invisible_pixel.gif" alt="" width="4" height="1" border="0" /></div>';
-
+    print '<div align="right"><img src="themes/'.$theme_name.'/images/invisible_pixel.gif" alt="" width="4" height="20" border="0" /><br />'.$reads.'<img 
+	src="themes/'.$theme_name.'/images/invisible_pixel.gif" alt="" width="4" height="1" border="0" /></div>';
     print '</td>';
     print '<td width="25"></td>';
     print '<td width="15" background="'.HTTPS.'themes/'.$theme_name.'/tables/CloseTable/rightside.png"></td>';
@@ -79,7 +69,5 @@ function themearticle($aid, $informant, $datetime, $modified, $title, $counter, 
     print '<td align="right" width="39" colspan="2"><img src="'.HTTPS.'themes/'.$theme_name.'/tables/CloseTable/bottomrightcorner.png"></td>';
     print '</tr>';
     print '</table>';
-
     print '<img src="'.HTTPS.'themes/'.$theme_name.'/images/invisible_pixel.gif" height=6><br>'."\n";
-
 }
