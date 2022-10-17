b568a28f684ea85d2ff3739dc4d86bb0efd46245
Ernest Buffington <ernest.buffington@gmail.com>
2022-10-17 08:00:52 -0400

    Update theme.php

--- themes/XtremeV3b/theme.php
+++ themes/XtremeV3b/theme.php
@@ -103,26 +103,35 @@ function FormatStory($thetext, $notes, $aid, $informant)
 global $anonymous;
 
 $notes = !empty($notes) ? '<br /><br /><strong>'._NOTE.'</strong> <em>'.$notes.'</em>' : '';	
-
-if ($aid == $informant): 
+if ($aid == $informant) 
+{
    echo '<span class="content" color="#505050">'.$thetext.$notes.'</span>';
-else: 
-   if (defined('WRITES')): 
-
-     if (!empty($informant)): 
+} 
+else 
+{
+   if (defined('WRITES')) 
+   {
+      if (!empty($informant)) 
+      {
          if ( is_array($informant) ):
             $boxstuff = '<a href="modules.php?name=Your_Account&amp;op=userinfo&amp;username='.$informant[0].'">'.$informant[1].'</a>';
          else:
             $boxstuff = '<a href="modules.php?name=Your_Account&amp;op=userinfo&amp;username='.$informant.'">'.$informant.'</a>';
          endif;
-     else: 
+} 
+else 
+{
             $boxstuff = $anonymous.' ';
+      }
             $boxstuff .= _WRITES.' <em>'.$thetext.'</em>'.$notes;
-    endif; 
-else: 
+} 
+else 
+{
             $boxstuff .= $thetext . $notes;
+      }
       echo '<span class="content" color="#505050">' . $boxstuff . '</span>';
-endif;
+   }
+}
 
 /*----------------*/
 /* Header Section */
