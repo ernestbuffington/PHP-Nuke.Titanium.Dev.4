157319fe52cbcd942f4860a68b00c3b61df40f88
Ernest Buffington <ernest.buffington@gmail.com>
2022-10-19 17:36:59 -0400

    Update

--- themes/CarbinFiber-Red-Flames/css/CKeditor.php
+++ themes/CarbinFiber-Red-Flames/css/CKeditor.php
@@ -1,9 +1,9 @@
 <?php
 #---------------------------------------------------------------------------------------#
-# themes/CarbonFiber/css/body.php                                                       #
+# themes/Universal/css/body.php                                                         #
 #---------------------------------------------------------------------------------------#
 # THEME INFO                                                                            #
-# CarbinFiber Theme v1.0 (Fixed & Full Width)                                           #
+# Universal Theme v1.0 (Fixed & Full Width)                                           #
 #                                                                                       #
 # Final Build Date 08/17/2019 Saturday 7:40pm                                           #
 #                                                                                       #
@@ -35,6 +35,41 @@ Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
 
+/* "Source" button label */
+.cke_button__source_label,
+.cke_button__sourcedialog_label
+{
+	display: inline;
+}
 
+/* "Font Size" combo width */
+.cke_combo__fontsize .cke_combo_text
+{
+	width: 30px;
+}
+
+/* "Font Size" panel size */
+.cke_combopanel__fontsize
+{
+	width: 120px;
+}
+
+/* Editable regions */
+textarea.cke_source
+{
+	font-family: 'Courier New', Monospace;
+	font-size: small;
+	background-color: #000000; 
+	white-space: pre-wrap;
+	border: none;
+	padding: 0;
+	margin: 0;
+	display: block;
+}
+
+.cke_wysiwyg_frame, .cke_wysiwyg_div
+{
+	background-color: #fff;
+}
 <?
 
