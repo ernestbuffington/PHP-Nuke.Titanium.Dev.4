4cd254dc824ef06fc0b679fe54c3195461192a7f
Ernest Buffington <ernest.buffington@gmail.com>
2022-11-24 10:27:37 -0500

    Correct Spelling Error 11/24.2022 10:27 am Ernest Allen Buffington

--- themes/Titanium_Core/header.php
+++ themes/Titanium_Core/header.php
@@ -96,7 +96,7 @@ $scrollmsg .= '<img src="images/browsers/opera.png" align="top" height="16"> Tha
 class="blink-one">Opera Rocks!</span> ::: This browser is hauling ass and about to catch up with Chrome... :::';
 }
 
-$scrollmsg .= ' Ezekiel 25,17. "The path of the righteous man is beset of all sides by the iniquities of the selfish and the tyranny of evil me. Blessed is he who, in the name of the charity and goodwill, shepherds the weak through the valley of darkness, for he is truly his brother’s keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy my brothers. And you will know my name is the Lord when I lay my vengeance upon thee.” ::: ';
+$scrollmsg .= ' Ezekiel 25,17. "The path of the righteous man is beset of all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of the charity and goodwill, shepherds the weak through the valley of darkness, for he is truly his brother’s keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy my brothers. And you will know my name is the Lord when I lay my vengeance upon thee.” ::: ';
 
 $scrollmsg .= 'The current Beta release of PHP-Nuke Titanium is v'.NUKE_TITANIUM.' ::: ';
 
