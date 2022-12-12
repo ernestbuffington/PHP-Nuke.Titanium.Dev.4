7464faa85b7048b3db8598db3df19ce9227eef90
Ernest Buffington <ernest.buffington@gmail.com>
2022-12-12 08:52:30 -0500

    Update personal_message.php

--- modules/Evo_UserBlock/addons/personal_message.php
+++ modules/Evo_UserBlock/addons/personal_message.php
@@ -1,41 +1,49 @@
 <?php
 /*=======================================================================
- PHP-Nuke Titanium v3.0.0 : Enhanced PHP-Nuke Web Portal System
+ PHP-Nuke Titanium : Nuke-Evolution | Enhanced and Advnanced
  =======================================================================*/
 
 /************************************************************************
-   Nuke-Evolution: Server Info Administration
+   Nuke-Evolution    : Server Info Administration
+   PHP-Nuke Titanium : Server Info Administration
    ============================================
    Copyright (c) 2005 by The Nuke-Evolution Team
+   Copyright (c) 2022 by The PHP-Nuke Titanium Group
 
-   Filename      : personal_message.php
-   Author(s)     : Technocrat (www.Nuke-Evolution.com)
-   Version       : 1.0.0
+   Filename      : avatar.php
+   Author(s)     : Ernest Allen Buffington, Technocrat
+   Version       : 4.0.3
    Date          : 05.19.2005 (mm.dd.yyyy)
+   Last Update   : 12.12.2022 (mm.dd.yyyy)
 
    Notes         : Evo User Block Personal Message Module
 ************************************************************************/
 
-if(!defined('NUKE_EVO')) {
-   die ("Illegal File Access");
-}
+if(!defined('NUKE_EVO')):
+ die ("Illegal File Access");
+endif;
 
 global $evouserinfo_personal_message;
 
-function evouserinfo_personal_message () {
+function evouserinfo_personal_message() 
+{
     global $evouserinfo_personal_message, $evouserinfo_addons;
+
     $evouserinfo_personal_message = $evouserinfo_addons['personal_message_message'];
     
-    if (is_user()) {
+    if(is_user()): 
         global $userinfo;
         $uname = UsernameColor($userinfo['username']);
-    } else {
+	else: 
         $uname = $lang_evo_userblock['BLOCK']['GOOD_AFTERNOON']['ANON'];
-    }
-    global $sitename;
-    $evouserinfo_personal_message = str_replace('%site%', $sitename, $evouserinfo_personal_message );
+    endif;
+    
+	global $sitename;
+    
+	$evouserinfo_personal_message = str_replace('%site%', $sitename, $evouserinfo_personal_message );
     $evouserinfo_personal_message = str_replace('%name%', $uname, $evouserinfo_personal_message );
 }
 
 evouserinfo_personal_message();
+
 ?>
