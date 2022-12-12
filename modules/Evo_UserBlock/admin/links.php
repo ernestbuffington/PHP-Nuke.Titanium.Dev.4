ba842baa63b8dc6727d90f8e133c0038c2a36c6c
Ernest Buffington <ernest.buffington@gmail.com>
2022-12-12 10:28:13 -0500

    Update links.php

--- modules/Evo_UserBlock/admin/links.php
+++ modules/Evo_UserBlock/admin/links.php
@@ -1,14 +1,28 @@
 <?php
 /*=======================================================================
- PHP-Nuke Titanium v3.0.0 : Enhanced PHP-Nuke Web Portal System
+ PHP-Nuke Titanium : Nuke-Evolution | Enhanced and Advnanced
  =======================================================================*/
 
+/************************************************************************
+   Nuke-Evolution    : Server Info Administration
+   PHP-Nuke Titanium : Server Info Administration
+   ============================================
+   Copyright (c) 2005 by The Nuke-Evolution Team
+   Copyright (c) 2022 by The PHP-Nuke Titanium Group
+
+   Filename      : links.php
+   Author(s)     : Ernest Allen Buffington, Technocrat
+   Version       : 4.0.3
+   Date          : 05.19.2005 (mm.dd.yyyy)
+   Last Update   : 12.12.2022 (mm.dd.yyyy)
+   
 /*****[CHANGES]**********************************************************
 -=[Base]=-
       Nuke Patched                             v3.1.0       06/26/2005
  ************************************************************************/
 
 global $admin_file;
+
 adminmenu($admin_file.'.php?op=evo-userinfo', _EVO_USERINFO, 'evo-userinfo.png');
 
 ?>
