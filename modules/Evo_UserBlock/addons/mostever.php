63ef0b9e79bd00a171af415de95521616f5d30aa
Ernest Buffington <ernest.buffington@gmail.com>
2022-12-12 08:04:34 -0500

    Update mostever.php

--- modules/Evo_UserBlock/addons/mostever.php
+++ modules/Evo_UserBlock/addons/mostever.php
@@ -1,29 +1,34 @@
 <?php
 /*=======================================================================
- PHP-Nuke Titanium v3.0.0 : Enhanced PHP-Nuke Web Portal System
+ PHP-Nuke Titanium : Nuke-Evolution | Enhanced and Advnanced
  =======================================================================*/
 
 /************************************************************************
-   Nuke-Evolution: Server Info Administration
+   Nuke-Evolution    : Server Info Administration
+   PHP-Nuke Titanium : Server Info Administration
    ============================================
    Copyright (c) 2005 by The Nuke-Evolution Team
+   Copyright (c) 2022 by The PHP-Nuke Titanium Group
 
-   Filename      : mostever.php
-   Author(s)     : Technocrat (www.Nuke-Evolution.com)
-   Version       : 1.0.0
+   Filename      : avatar.php
+   Author(s)     : Ernest Allen Buffington, Technocrat
+   Version       : 4.0.3
    Date          : 05.19.2005 (mm.dd.yyyy)
-
+   Last Update   : 12.12.2022 (mm.dd.yyyy)
+   
    Notes         : Evo User Block Most Ever Online Module
 ************************************************************************/
 
-if(!defined('NUKE_EVO'))
-    die ("Illegal File Access");
+if(!defined('NUKE_EVO')):
+  die("Illegal File Access");
+endif;
 
 global $evouserinfo_addons, $evouserinfo_mostever;
 
-function evouserinfo_get_mostonline () 
+function evouserinfo_get_mostonline() 
 {
-    global $db, $prefix;    
+    global $db, $prefix;
+	    
     $result = $db->sql_query("SELECT total, members, nonmembers FROM ".$prefix."_mostonline");
     $mostonline = $db->sql_fetchrow($result);
     $db->sql_freeresult($result);
@@ -44,11 +49,9 @@ function evouserinfo_get_mostonline ()
 
     $total = $users + $guests;
     
-    if ($total > $out['total']):
-
+    if($total > $out['total']):
         $db->sql_query("DELETE FROM `".$prefix."_mostonline` WHERE `total`='".$out['total']."' LIMIT 1");
         $db->sql_query("INSERT INTO `".$prefix."_mostonline` VALUES ('".$total."','".$users."','".$guests."')");
-
     endif;
 
     return $out;
@@ -56,22 +59,23 @@ function evouserinfo_get_mostonline ()
 
 
 global $userinfo, $lang_evo_userblock;
+
 $block_mostever = evouserinfo_get_mostonline();
 
 $evouserinfo_mostever .= '<div style="font-weight: bold">'.$lang_evo_userblock['BLOCK']['MOST']['MOST'].'</div>';
 $evouserinfo_mostever .= '<div style="padding-left: 10px;">';
-$evouserinfo_mostever .= '<font color="gold"><i class="fa fa-pie-chart" aria-hidden="true"></i>
-</font>&nbsp;'.$lang_evo_userblock['BLOCK']['ONLINE']['GUESTS'].'<span style="float:right">'.number_format($block_mostever['nonmembers']).'&nbsp;&nbsp;</span>';
+$evouserinfo_mostever .= '<span style="color: gold;"><i class="fa fa-pie-chart" aria-hidden="true"></i>
+</span>&nbsp;'.$lang_evo_userblock['BLOCK']['ONLINE']['GUESTS'].'<span style="float:right">'.number_format($block_mostever['nonmembers']).'&nbsp;&nbsp;</span>';
 $evouserinfo_mostever .= '</div>';
 
 $evouserinfo_mostever .= '<div style="padding-left: 10px;">';
-$evouserinfo_mostever .= '<font color="#FF3300"><i class="fa fa-pie-chart" aria-hidden="true"></i>
-</font>&nbsp;'.$lang_evo_userblock['BLOCK']['ONLINE']['MEMBERS'].'<span style="float:right">'.number_format($block_mostever['members']).'&nbsp;&nbsp;</span>';
+$evouserinfo_mostever .= '<span style="color: #FF3300;"><i class="fa fa-pie-chart" aria-hidden="true"></i>
+</span>&nbsp;'.$lang_evo_userblock['BLOCK']['ONLINE']['MEMBERS'].'<span style="float:right">'.number_format($block_mostever['members']).'&nbsp;&nbsp;</span>';
 $evouserinfo_mostever .= '</div>';
 
 $evouserinfo_mostever .= '<div style="padding-left: 10px;">';
-$evouserinfo_mostever .= '<font color="pink"><i class="fa fa-pie-chart" aria-hidden="true"></i>
-</font>&nbsp;'.$lang_evo_userblock['BLOCK']['ONLINE']['TOTAL'].'<span style="float:right">'.number_format($block_mostever['total']).'&nbsp;&nbsp;</span>';
+$evouserinfo_mostever .= '<span style="color: pink;"><i class="fa fa-pie-chart" aria-hidden="true"></i>
+</span>&nbsp;'.$lang_evo_userblock['BLOCK']['ONLINE']['TOTAL'].'<span style="float:right">'.number_format($block_mostever['total']).'&nbsp;&nbsp;</span>';
 $evouserinfo_mostever .= '</div>';
 
 ?>
