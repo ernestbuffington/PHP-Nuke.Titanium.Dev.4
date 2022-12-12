3ceeca340c581ad5fcf98413911d38d47036c707
Ernest Buffington <ernest.buffington@gmail.com>
2022-12-12 07:19:51 -0500

    Update avatar.php

--- modules/Evo_UserBlock/addons/avatar.php
+++ modules/Evo_UserBlock/addons/avatar.php
@@ -1,41 +1,37 @@
 <?php
 /*=======================================================================
- PHP-Nuke Titanium v3.0.0 : Enhanced PHP-Nuke Web Portal System
+ PHP-Nuke Titanium : Nuke-Evolution | Enhanced and Advnanced
  =======================================================================*/
 
 /************************************************************************
-   Nuke-Evolution: Server Info Administration
+   Nuke-Evolution    : Server Info Administration
+   PHP-Nuke Titanium : Server Info Administration
    ============================================
    Copyright (c) 2005 by The Nuke-Evolution Team
+   Copyright (c) 2022 by The PHP-Nuke Titanium Group
 
    Filename      : avatar.php
-   Author(s)     : Technocrat (www.Nuke-Evolution.com)
-   Version       : 1.0.0
+   Author(s)     : Ernest Allen Buffington, Technocrat
+   Version       : 4.0.3
    Date          : 05.19.2005 (mm.dd.yyyy)
+   Last Update   : 12.12.2022 (mm.dd.yyyy)
 
    Notes         : Evo User Block Avatar Module
 ************************************************************************/
 
-if(!defined('NUKE_EVO')) 
-{
+if(!defined('NUKE_EVO')): 
    die ("Illegal File Access");
-}
-
+endif;
 global $avatar_overide_size, $make_xtreme_avatar_small, $board_config, $userinfo;
-// START - this was added for the very whimpy small themes that have no block width! by Ernest Buffington 08/06/2019
-if ($make_xtreme_avatar_small === true)
-{
+# START - this was added for the very whimpy small themes that have no block width! by Ernest Buffington 08/06/2019
+if($make_xtreme_avatar_small === true):
   $board_config['avatar_max_height'] = '';
   $board_config['avatar_max_width'] = $avatar_overide_size;
-}
-// END - this was added for the very whimpy small themes that have no block width! by Ernest Buffington 08/06/2019
-
+endif;
+# END - this was added for the very whimpy small themes that have no block width! by Ernest Buffington 08/06/2019
 $evouserinfo_avatar = '<div style="text-align:center">';
-
-if (is_user() && $userinfo['user_avatar'])
-{
-	switch( $userinfo['user_avatar_type'])
-	{
+if(is_user() && $userinfo['user_avatar']):
+	switch($userinfo['user_avatar_type']):
 		# user_allowavatar = 1
 		case USER_AVATAR_UPLOAD:
 			$evouserinfo_avatar .= ( $board_config['allow_avatar_upload'] ) 
@@ -53,11 +49,11 @@ if (is_user() && $userinfo['user_avatar'])
 			.$board_config['avatar_max_width'].'px;" src="' . $board_config['avatar_gallery_path'] . '/' . (($userinfo['user_avatar'] == 'blank.png' || $userinfo['user_avatar'] 
 			== 'gallery/blank.png') ? 'blank.png' : $userinfo['user_avatar']) . '" alt="" border="0" />' : '';
 			break;
-   }
-}
-else
+   endswitch;
+else:
 $evouserinfo_avatar .= '<img class="rounded-corners-user-info" style="max-height: '.$board_config['avatar_max_height'].'px; max-width: '
 .$board_config['avatar_max_width'].'px;" src="'.$board_config['default_avatar_users_url'].'" alt="" border="0" />';
-
+endif;
 $evouserinfo_avatar .= '</div>';
+
 ?>
