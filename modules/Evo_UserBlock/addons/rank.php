59432bfea134fb382792176248cf6ee171e5d1e6
Ernest Buffington <ernest.buffington@gmail.com>
2022-12-12 09:13:16 -0500

    Update rank.php

--- modules/Evo_UserBlock/addons/rank.php
+++ modules/Evo_UserBlock/addons/rank.php
@@ -1,27 +1,34 @@
 <?php
 /*=======================================================================
- PHP-Nuke Titanium v3.0.0 : Enhanced PHP-Nuke Web Portal System
+ PHP-Nuke Titanium : Nuke-Evolution | Enhanced and Advnanced
  =======================================================================*/
+
 /************************************************************************
-   Nuke-Evolution: Server Info Administration
+   Nuke-Evolution    : Server Info Administration
+   PHP-Nuke Titanium : Server Info Administration
    ============================================
    Copyright (c) 2005 by The Nuke-Evolution Team
+   Copyright (c) 2022 by The PHP-Nuke Titanium Group
 
-   Filename      : rank.php
-   Author(s)     : Technocrat (www.Nuke-Evolution.com)
-   Version       : 1.0.0
+   Filename      : avatar.php
+   Author(s)     : Ernest Allen Buffington, Technocrat
+   Version       : 4.0.3
    Date          : 05.19.2005 (mm.dd.yyyy)
-
+   Last Update   : 12.12.2022 (mm.dd.yyyy)
+   
    Notes         : Evo User Block User Rank Module
 ************************************************************************/
-if(!defined('NUKE_EVO'))
-exit ("Illegal File Access");
+
+if(!defined('NUKE_EVO')):
+  exit ("Illegal File Access");
+endif;
 
 global $evouserinfo_rank;
 
 function evouserinfo_rank() 
 {
-    global $evouserinfo_addons, $db, $prefix, $userinfo, $evouserinfo_rank;
+   global $evouserinfo_addons, $db, $prefix, $userinfo, $evouserinfo_rank;
+  
    # Mod: Multiple Ranks And Staff View v2.0.3 START
    require_once(NUKE_INCLUDE_DIR.'functions_mg_ranks.php');
    $ranks_sql = query_ranks();
@@ -47,23 +54,29 @@ function evouserinfo_rank()
    $evouserinfo_rank  = '<div style="text-align: center">';
    $evouserinfo_rank .= $user_rank_01_img.$user_rank_01;
    
-   if($evouserinfo_addons['rank_show_2nd'] == 'yes')
-   $evouserinfo_rank .= $user_rank_02_img.$user_rank_02;
+   if($evouserinfo_addons['rank_show_2nd'] == 'yes'):
+     $evouserinfo_rank .= $user_rank_02_img.$user_rank_02;
+   endif;
    
-   if($evouserinfo_addons['rank_show_3rd'] == 'yes')
-   $evouserinfo_rank .= $user_rank_03_img.$user_rank_03;
+   if($evouserinfo_addons['rank_show_3rd'] == 'yes'):
+     $evouserinfo_rank .= $user_rank_03_img.$user_rank_03;
+   endif;
    
-   if($evouserinfo_addons['rank_show_4th'] == 'yes')
-   $evouserinfo_rank .= $user_rank_04_img.$user_rank_04;
+   if($evouserinfo_addons['rank_show_4th'] == 'yes'):
+     $evouserinfo_rank .= $user_rank_04_img.$user_rank_04;
+   endif;
    
-   if($evouserinfo_addons['rank_show_5th'] == 'yes')
-   $evouserinfo_rank .= $user_rank_05_img.$user_rank_05;
+   if($evouserinfo_addons['rank_show_5th'] == 'yes'):
+     $evouserinfo_rank .= $user_rank_05_img.$user_rank_05;
+   endif;
    
    $evouserinfo_rank .= '</div>';
 }
 
-if(is_user()) 
+if(is_user()):
 evouserinfo_rank();
-else 
-$evouserinfo_rank = '';
+else: 
+  $evouserinfo_rank = '';
+endif;
+
 ?>
