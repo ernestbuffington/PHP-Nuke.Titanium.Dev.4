a55deaf0d54e9edd0d108dbc799f87e1ce8e652c
Ernest Buffington <ernest.buffington@gmail.com>
2022-12-12 10:29:49 -0500

    Update functions.php

--- modules/Evo_UserBlock/admin/includes/functions.php
+++ modules/Evo_UserBlock/admin/includes/functions.php
@@ -1,7 +1,23 @@
 <?php
 /*=======================================================================
- PHP-Nuke Titanium v3.0.0 : Enhanced PHP-Nuke Web Portal System
+ PHP-Nuke Titanium : Nuke-Evolution | Enhanced and Advnanced
  =======================================================================*/
+
+/************************************************************************
+   Nuke-Evolution    : Server Info Administration
+   PHP-Nuke Titanium : Server Info Administration
+   ============================================
+   Copyright (c) 2005 by The Nuke-Evolution Team
+   Copyright (c) 2022 by The PHP-Nuke Titanium Group
+
+   Filename      : functions.php
+   Author(s)     : Ernest Allen Buffington, Technocrat
+   Version       : 4.0.3
+   Date          : 05.19.2005 (mm.dd.yyyy)
+   Last Update   : 12.12.2022 (mm.dd.yyyy)
+
+   Notes         : Evo User Block Online Administration
+************************************************************************/
 
 if (!defined('ADMIN_FILE')) {
    die ("Illegal File Access");
