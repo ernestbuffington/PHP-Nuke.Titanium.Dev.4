33923fdac9e7ad03c32cf8824db298c157ba05a4
Ernest Buffington <ernest.buffington@gmail.com>
2022-12-12 15:44:14 -0500

    Update rector.php

--- rector.php
+++ rector.php
@@ -84,7 +84,7 @@ return static function (RectorConfig $rectorConfig): void {
 		// done fixed __DIR__ . '/modules/Forums/glance.php',
 		//__DIR__ . '/includes/functions.php',
 		// done fixed __DIR__ . '/modules/Forums/search.php',
-       __DIR__ . '/modules/Your_Account/public/new_user3.php',
+       __DIR__ . '/modules/Your_Account/public/new_user2.php',
         //
     ]);
 
