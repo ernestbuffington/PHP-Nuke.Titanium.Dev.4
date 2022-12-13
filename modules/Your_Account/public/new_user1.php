e5515b113f39bab23a58d017b7a2cfe9528a319c
Ernest Buffington <ernest.buffington@gmail.com>
2022-12-13 04:54:26 -0500

    Update new_user1.php

--- modules/Your_Account/public/new_user1.php
+++ modules/Your_Account/public/new_user1.php
@@ -3,7 +3,6 @@
   PHP-Nuke Titanium | Nuke-Evolution Xtreme : PHP-Nuke Web Portal System
  =======================================================================*/
 
-
 /*********************************************************************************/
 /* CNB Your Account: An Advanced User Management System for phpnuke             */
 /* ============================================                                 */
@@ -19,7 +18,7 @@
 /* the Free Software Foundation; either version 2 of the License.               */
 /*                                                                              */
 /*********************************************************************************/
-/* CNB Your Account it the official successor of NSN Your Account by Bob Marion    */
+/* CNB Your Account it the official successor of NSN Your Account by Bob Marion  */
 /*********************************************************************************/
 
 /*****[CHANGES]**********************************************************
@@ -52,18 +51,26 @@ if (!defined('CNBYA')) {
     block_vpn_proxy_user();
     title(_USERAPPLOGIN);
     OpenTable();
+	
+	print '<div align="center" style="padding-top:14px;">';
+    print '</div>';
+	
     echo "<form action='modules.php?name=$module_name' method='post' name='newuser'>\n";
-    echo "<table style='margin:auto' cellpadding='3' cellspacing='3' border='0'>\n";
+    echo "<table class=\"rounded-corners\" style='margin:auto' cellpadding='3' cellspacing='3' border='0'>\n";
     echo "<tr><td align='center' bgcolor='$bgcolor1' colspan='2'><div class=\"textbold\">"._REGNEWUSER."</div></td></tr>\n";
-    echo "<tr><td bgcolor='$bgcolor2'><div class=\"textbold\">"._NICKNAME.":</div>"._REQUIRED."</td><td bgcolor='$bgcolor1'><input type='text' name='ya_username' id='username_input' size='15' maxlength='".$ya_config['nick_max']."'>&nbsp;<span id=\"username_check_result\"></span><br /><span class='tiny'>("._YA_NICKLENGTH.")</span></td></tr>\n";
+   
+    echo '<tr><td style="background-color: '.$bgcolor1.';">&nbsp;</td>'.PHP_EOL;
+	echo '<td style="background-color: '.$bgcolor1.';">&nbsp;</td></tr>'.PHP_EOL;   
+    
+	echo "<tr><td style=\"background-color: $bgcolor2;\"><div class=\"textbold\">"._NICKNAME.":</div>"._REQUIRED."</td><td style=\"background-color: $bgcolor1;\"><input type='text' name='ya_username' id='username_input' size='15' maxlength='".$ya_config['nick_max']."'>&nbsp;<span id=\"username_check_result\"></span><br /><span class='tiny'>("._YA_NICKLENGTH.")</span></td></tr>\n";
 // menelaos: by request: added realname to the registration form
-    echo "<tr><td bgcolor='$bgcolor2'><div class=\"textbold\">"._UREALNAME.":</div>"._OPTIONAL."</td><td bgcolor='$bgcolor1'><input type='text' name='ya_realname' size='40' maxlength='60'><br /><span class='tiny'>"._YA_REALNAMENOTE."</span></td></tr>\n";
+    echo "<tr><td style=\"background-color: $bgcolor2;\"><div class=\"textbold\">"._UREALNAME.":</div>"._OPTIONAL."</td><td style=\"background-color: $bgcolor1;\"><input type='text' name='ya_realname' size='40' maxlength='60'><br /><span class='tiny'>"._YA_REALNAMENOTE."</span></td></tr>\n";
 
-    echo "<tr><td bgcolor='$bgcolor2'><div class=\"textbold\">"._EMAIL.":</div>"._REQUIRED."</td><td bgcolor='$bgcolor1'><input type='text' name='ya_user_email' size='40' maxlength='255'>&nbsp;<span class='tiny'></span></td></tr>\n";
+    echo "<tr><td style=\"background-color: $bgcolor2;\"><div class=\"textbold\">"._EMAIL.":</div>"._REQUIRED."</td><td style=\"background-color: $bgcolor1;\"><input type='text' name='ya_user_email' size='40' maxlength='255'>&nbsp;<span class='tiny'></span></td></tr>\n";
 
     // menelaos: added configurable doublecheck email routine
     if ($ya_config['doublecheckemail']==1) {
-    echo "<tr><td bgcolor='$bgcolor2'><div class=\"textbold\">"._RETYPEEMAIL.":</div></td><td bgcolor='$bgcolor1'><input type='text' name='ya_user_email2' size='40' maxlength='255'></td></tr>\n";
+    echo "<tr><td style=\"background-color: $bgcolor2;\"><div class=\"textbold\">"._RETYPEEMAIL.":</div></td><td style=\"background-color: $bgcolor1;\"><input type='text' name='ya_user_email2' size='40' maxlength='255'></td></tr>\n";
     } else {
     echo "<input type='hidden' name='ya_user_email2' value='ya_user_email'>\n";
     }
@@ -76,13 +83,13 @@ if (!defined('CNBYA')) {
     $result = $db->sql_query("SELECT * FROM ".$user_prefix."_cnbya_field WHERE (need = '2') OR (need = '3') ORDER BY pos");
         while ($sqlvalue = $db->sql_fetchrow($result)) {
           $t = $sqlvalue['fid'];
-          $value2 = explode("::", $sqlvalue['value']);
-          if (substr($sqlvalue['name'],0,1)=='_') eval( "\$name_exit = $sqlvalue[name];"); else $name_exit = $sqlvalue['name'];
+          $value2 = explode("::", (string) $sqlvalue['value']);
+          if (substr((string) $sqlvalue['name'],0,1)=='_') eval( "\$name_exit = $sqlvalue[name];"); else $name_exit = $sqlvalue['name'];
           if (count($value2) == 1) {
-            echo "<tr><td bgcolor='$bgcolor2'>$name_exit</td><td bgcolor='$bgcolor3'>";
+            echo "<tr><td style=\"background-color: $bgcolor2;\">$name_exit</td><td style=\"background-color: $bgcolor3;\">";
             echo "<input type='text' name='nfield[$t]' size='20' maxlength='$sqlvalue[size]'>\n";
             } else {
-            echo "<tr><td bgcolor='$bgcolor2'>$name_exit</td><td bgcolor='$bgcolor3'>";
+            echo "<tr><td style=\"background-color: $bgcolor2;\">$name_exit</td><td style=\"background-color: $bgcolor3;\">";
             echo "<select name='nfield[$t]'>\n";
                 for ($i = 0; $i<count($value2); $i++) {
                 echo "<option value=\"".trim($value2[$i])."\">".trim($value2[$i])."</option>\n";
@@ -92,7 +99,7 @@ if (!defined('CNBYA')) {
             if (($sqlvalue['need']) > 1) echo"&nbsp;<span class='tiny'>"._REQUIRED."</span>";
               echo "</td></tr>\n";
         }
-    echo "<tr><td bgcolor='$bgcolor2'><div class=\"textbold\">"._PASSWORD.":</div></td><td bgcolor='$bgcolor1'><input type='password' name='user_password' size='10' maxlength='".$ya_config['pass_max']."' onkeyup='chkpwd(newuser.user_password.value)' onblur='chkpwd(newuser.user_password.value)' onmouseout='chkpwd(newuser.user_password.value)'>";
+    echo "<tr><td style=\"vertical-align: top; background-color: $bgcolor2;\"><div class=\"textbold\">"._PASSWORD.":</div></td><td style=\"background-color: $bgcolor1;\"><input type='password' name='user_password' size='10' maxlength='".$ya_config['pass_max']."' onkeyup='chkpwd(newuser.user_password.value)' onblur='chkpwd(newuser.user_password.value)' onmouseout='chkpwd(newuser.user_password.value)'>";
 /*****[BEGIN]******************************************
  [ Mod:     Password Strength Meter            v1.0.0 ]
  ******************************************************/
@@ -108,24 +115,30 @@ if (!defined('CNBYA')) {
  [ Mod:     Password Strength Meter            v1.0.0 ]
  ******************************************************/
     echo "<br /><span class='tiny'>("._BLANKFORAUTO.")</span><br /><span class='tiny'>("._YA_PASSLENGTH.")</span></td></tr>\n";
-    echo "<tr><td bgcolor='$bgcolor2'><div class=\"textbold\">"._RETYPEPASSWORD.":</div></td><td bgcolor='$bgcolor1'><input type='password' name='user_password2' size='10' maxlength='".$ya_config['pass_max']."'><br /><span class='tiny'>("._BLANKFORAUTO.")</span><br /><span class='tiny'>("._YA_PASSLENGTH.")</span></td></tr>\n";
-    $gfxchk = array(3,4,6,7);
-    $gfx = security_code($gfxchk, 'normal'); //Size - compact || normal
-    if(!empty($gfx)) {
-        echo "<tr><td bgcolor='$bgcolor2' colspan='2' style='text-align:center;'>".$gfx."</td></tr>";
+    echo "<tr><td style=\"vertical-align: top; background-color: $bgcolor2;\"><div class=\"textbold\">"._RETYPEPASSWORD.":</div></td><td style=\"background-color: $bgcolor1;\"><input type='password' name='user_password2' size='10' maxlength='".$ya_config['pass_max']."'><br /><span class='tiny'>("._BLANKFORAUTO.")</span><br /><span class='tiny'>("._YA_PASSLENGTH.")</span></td></tr>\n";
+    
+	$gfxchk = [3, 4, 6, 7];
+    
+	$gfx = security_code($gfxchk, 'normal'); //Size - compact || normal
+    
+	if(!empty($gfx)) {
+        echo "<tr><td style=\"background-color: $bgcolor2;\" colspan='2' style='text-align:center;'><div align=\"center\">".$gfx."</div></td></tr>";
     }
+	
 	# start Nuke Honeypot
 	if (file_exists('./includes/honeypot/hp_new_user2.php')) {
 	include_once './includes/honeypot/hp_new_user2.php';
 	}
 	# end Nuke Honeypot	
     echo "<input type='hidden' name='op' value='new_confirm'>\n";
-    echo "<tr><td align='right' bgcolor='$bgcolor1' colspan='2'><input type='submit' value='"._YA_CONTINUE."'></td></tr>\n";
+    echo "<tr><td align='right' style=\"background-color: $bgcolor1;\" colspan='2'><input type='submit' value='"._YA_CONTINUE."'></td></tr>\n";
     echo "</table></form>\n";
     echo "<br />\n";
     echo _WAITAPPROVAL."<br /><br />\n";
     echo _COOKIEWARNING."<br />\n";
-    echo _ASREGUSER."<br />\n";
+    
+	/*
+	echo _ASREGUSER."<br />\n";
     echo "<ul>\n";
     echo "<li>"._ASREG1."\n";
     echo "<li>"._ASREG2."\n";
@@ -156,7 +169,9 @@ if (!defined('CNBYA')) {
     echo "</ul>\n";
     echo _REGISTERNOW."<br />\n";
     echo _WEDONTGIVE."<br /><br />\n";
-    CloseTable();
+    
+	*/
+	CloseTable();
     include_once(NUKE_BASE_DIR.'footer.php');
 
 ?>
