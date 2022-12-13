c02b58b5ee0d570bfcbdced0f4dc46a32b34b788
Ernest Buffington <ernest.buffington@gmail.com>
2022-12-13 11:24:15 -0500

    Update rector.php

--- rector.php
+++ rector.php
@@ -85,7 +85,7 @@ return static function (RectorConfig $rectorConfig): void {
 		//__DIR__ . '/includes/usercp_avatar.php',
 		//__DIR__ . '/modules/Blogs/comments.php',
 		//__DIR__ . '/includes/functions.php',
-		__DIR__ . '/modules/Forums/admin/admin_users.php',
+		__DIR__ . '/modules/Forums/admin/admin_xdata_auth.php',
        //__DIR__ . '/modules/Your_Account/public/new_user1.php',
         //
     ]);
