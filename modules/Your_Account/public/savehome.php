6928d6a9bbb370dcb9e0cd829ff80b1b717b9182
Ernest Buffington <ernest.buffington@gmail.com>
2022-12-13 09:57:37 -0500

    PHP 8.1 Patched 12/12/2022 9:57 am Ernest Allen Buffington

--- modules/Your_Account/public/savehome.php
+++ modules/Your_Account/public/savehome.php
@@ -3,37 +3,39 @@
   PHP-Nuke Titanium | Nuke-Evolution Xtreme : PHP-Nuke Web Portal System
  =======================================================================*/
 
+/*********************************************************************************/
+/* CNB Your Account: An Advanced User Management System for phpnuke              */
+/* ============================================                                  */
+/*                                                                               */
+/* Copyright (c) 2004 by Comunidade PHP Nuke Brasil                              */
+/* http://dev.phpnuke.org.br & http://www.phpnuke.org.br                         */
+/*                                                                               */
+/* Contact author: escudero@phpnuke.org.br                                       */
+/* International Support Forum: http://ravenphpscripts.com/forum76.html          */
+/*                                                                               */
+/* This program is free software. You can redistribute it and/or modify          */
+/* it under the terms of the GNU General Public License as published by          */
+/* the Free Software Foundation; either version 2 of the License.                */
+/*                                                                               */
+/*********************************************************************************/
+/* CNB Your Account it the official successor of NSN Your Account by Bob Marion  */
+/*********************************************************************************/
 
-/*********************************************************************************/
-/* CNB Your Account: An Advanced User Management System for phpnuke             */
-/* ============================================                                 */
-/*                                                                              */
-/* Copyright (c) 2004 by Comunidade PHP Nuke Brasil                             */
-/* http://dev.phpnuke.org.br & http://www.phpnuke.org.br                        */
-/*                                                                              */
-/* Contact author: escudero@phpnuke.org.br                                      */
-/* International Support Forum: http://ravenphpscripts.com/forum76.html         */
-/*                                                                              */
-/* This program is free software. You can redistribute it and/or modify         */
-/* it under the terms of the GNU General Public License as published by         */
-/* the Free Software Foundation; either version 2 of the License.               */
-/*                                                                              */
-/*********************************************************************************/
-/* CNB Your Account it the official successor of NSN Your Account by Bob Marion    */
-/*********************************************************************************/
-
-/*****[CHANGES]**********************************************************
+/*****[CHANGES]*******************************************************************
 -=[Base]=-
       Nuke Patched                             v3.1.0       06/26/2005
- ************************************************************************/
+	  PHP 8.1 Patched                          v4.0.3       12/12/2022
+-=[Last Updated]=-
+      12/12/2022 9:50 am Ernest ALlen Buffington	  
+ *********************************************************************************/
 
-if (!defined('MODULE_FILE')) {
-   die ("Access Denied");
-}
+if (!defined('MODULE_FILE')):
+ die ("Access Denied");
+endif;
 
-if (!defined('CNBYA')) {
-    die('CNBYA protection');
-}
+if(!defined('CNBYA')):
+ die('CNBYA protection');
+endif;
 
     global $cookie, $userinfo;
     $check = $cookie[1];
@@ -42,10 +44,25 @@ if (!defined('CNBYA')) {
     $row = $db->sql_fetchrow($result);
     $vuid = $row['user_id'];
     $ccpass = $row['user_password'];
-    if (($user_id == $vuid) AND ($check2 == $ccpass)) {
+ 
+    if(($user_id == $vuid) AND ($check2 == $ccpass)): 
+	
         $db->sql_query("UPDATE ".$user_prefix."_users SET storynum='$storynum' WHERE user_id='$user_id'");
-        yacookie($userinfo[user_id],$userinfo[username],$userinfo[user_password],$userinfo[storynum],$userinfo[umode],$userinfo[uorder],$userinfo[thold],$userinfo[noscore],$userinfo[ublockon],$userinfo[theme],$userinfo[commentmax]);
+    
+	    yacookie($userinfo['user_id'],
+		        $userinfo['username'],
+		   $userinfo['user_password'],
+		        $userinfo['storynum'],
+				   $userinfo['umode'],
+				  $userinfo['uorder'],
+				   $userinfo['thold'],
+				 $userinfo['noscore'],
+				$userinfo['ublockon'],
+				   $userinfo['theme'],
+			  $userinfo['commentmax']);
+			  
         redirect("modules.php?name=$module_name");
-    }
+		
+    endif;
 
 ?>
