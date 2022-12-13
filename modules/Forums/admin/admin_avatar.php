c003cc81bbe0ebc42e71e5ffaa27b5e325e28601
Ernest Buffington <ernest.buffington@gmail.com>
2022-12-13 09:33:15 -0500

    Update admin_avatar.php

--- modules/Forums/admin/admin_avatar.php
+++ modules/Forums/admin/admin_avatar.php
@@ -3,7 +3,6 @@
   PHP-Nuke Titanium | Nuke-Evolution Xtreme : PHP-Nuke Web Portal System
  =======================================================================*/
 
-
 /***************************************************************************
  *                              avatar_manage.php
  *                            -------------------
@@ -72,34 +71,33 @@
 
 define('IN_PHPBB', 1);
 
-if( !empty($setmodules) )
-{
-        $file = basename(__FILE__);
-        $module['General']['Avatar_Management'] = "$file";
-        return;
-}
+if(!empty($setmodules)):
+  $file = basename(__FILE__);
+  $module['General']['Avatar_Management'] = "$file";
+  return;
+endif;
 
 $root_path = "./../../../";
 $phpbb_root_path = './../';
+
 require($phpbb_root_path . 'extension.inc');
+
 require('pagestart.' . $phpEx);
 
-//include($root_path . 'includes/constants.'.$phpEx);
-//include($root_path . 'includes/db.'.$phpEx);
-// Any mode passed?
-if( isset($HTTP_GET_VARS['mode']) || isset($HTTP_POST_VARS['mode']) )
-{
-        $mode = ( isset($HTTP_GET_VARS['mode']) ) ? $HTTP_GET_VARS['mode'] : $HTTP_POST_VARS['mode'];
-    $target = ( isset($HTTP_GET_VARS['target']) ) ? $HTTP_GET_VARS['target'] : $HTTP_POST_VARS['target'];
-}
-else
-{
-        $mode = "";
-}
+# Any mode passed?
+if(isset($_GET['mode']) || isset($_POST['mode'])):
+  $mode = $_GET['mode'] ?? $_POST['mode'];
+  $target = $_GET['target'] ?? $_POST['target'];
+else:
+  $mode = "";
+endif;
 
-// Select all avatars and usernames that have an uploaded avatar currently
-$sql = "SELECT user_id, username, user_avatar FROM " . USERS_TABLE . "
-    WHERE user_avatar_type = " . USER_AVATAR_UPLOAD . " AND user_avatar IS NOT NULL";
+# Select all avatars and usernames that have an uploaded avatar currently
+$sql = "SELECT user_id, username, user_avatar FROM ".USERS_TABLE."
+
+WHERE user_avatar_type = ".USER_AVATAR_UPLOAD." 
+
+AND user_avatar IS NOT NULL";
 
 if(!$result = $db->sql_query($sql))
 {
@@ -107,20 +105,15 @@ if(!$result = $db->sql_query($sql))
     die("Could not get avatar information! $error[code] : $error[message]");
 }
 
-// Create a hash to keep track of all the user that is using the uploaded avatar
-while ($avatar_rowset = $db->sql_fetchrow($result))
-{
-/*****[BEGIN]******************************************
- [ Mod:    Advanced Username Color             v1.0.5 ]
- ******************************************************/
-    $avatar_usage[$avatar_rowset[user_avatar]] = UsernameColor($avatar_rowset[username]);
-/*****[END]********************************************
- [ Mod:    Advanced Username Color             v1.0.5 ]
- ******************************************************/  
-}
+# Create a hash to keep track of all the user that is using the uploaded avatar
+while($avatar_rowset = $db->sql_fetchrow($result)):
+    # Mod: Advanced Username Color v1.0.5 START
+    $avatar_usage[$avatar_rowset['user_avatar']] = UsernameColor($avatar_rowset['username']);
+    # Mod: Advanced Username Color v1.0.5 END
+endwhile;
 
-// This is the variable that points to the path of the avatars
-// You may need to ajust this to meet your needs ;)
+# This is the variable that points to the path of the avatars
+# You may need to ajust this to meet your needs ;)
 $real_avatar_dir = $phpbb_root_path . '../../' . $board_config['avatar_path'];
 
 echo '<h1>Avatar Management</h1>
@@ -131,19 +124,16 @@ by any user an option to delete it will appear.  This is a permanent delete, onc
 avatar it will be removed from the server and no longer be available.  You will be prompted for confirmation (requires
 Javascript).</p>';
 
-switch( $mode )
-{
-        case "delete":
-        echo '<table cellpadding=4 cellspacing=1 border=0 class=forumline>';
-        if ( unlink($real_avatar_dir.'/'.$target) )
-        {
-            print "<tr><td>Success, $target deleted!</td></tr><tr><td><a href=\"./admin_avatar.php\">Continue</a></td></tr></table>";
-        }
-        else
-        {
-            print "<tr><td>FAILED to delete $target!</td></tr><tr><td><a href=javascript:history.go(-1)>Go Back</a></td></tr></table>";
-        }
-        break;
+switch($mode):
+
+  case "delete":
+    echo '<table cellpadding=4 cellspacing=1 border=0 class=forumline>';
+      if(unlink($real_avatar_dir.'/'.$target)):
+        print "<tr><td>Success, $target deleted!</td></tr><tr><td><a href=\"./admin_avatar.php\">Continue</a></td></tr></table>";
+      else:
+        print "<tr><td>FAILED to delete $target!</td></tr><tr><td><a href=javascript:history.go(-1)>Go Back</a></td></tr></table>";
+      endif;
+      break;
 
     default:
         echo '<style>
@@ -166,65 +156,69 @@ switch( $mode )
         $alt2 = '#EEEEEE';
         $alter = $alt2;
 
-        // This is where we go through the avatar directory and report whether they are not
-        // used or if they are used, by who.
-        if ($avatar_dir = @opendir($real_avatar_dir))
-        {
-            while( $file = @readdir($avatar_dir) )
-            {
-                // This is where the script will filter out any file that doesn't match the patterns
-                if( $file != "." && $file != ".." && preg_match("/\.(gif|jpg|jpeg|png)$/i",$file) )
-                {
+        # This is where we go through the avatar directory and report whether they are not
+        # used or if they are used, by who.
+        if($avatar_dir = opendir($real_avatar_dir)):
+        
+            while($file = readdir($avatar_dir)):
+            
+                # This is where the script will filter out any file that doesn't match the patterns
+                if($file != "." && $file != ".." && preg_match("/\.(gif|jpg|webp|jpeg|png)$/i",$file)):
+                
                     $stats = stat($real_avatar_dir.'/'.$file);
 
-                    // Alternating row colows code
-                    if     ($alter == $alt1) { $alter = $alt2; }
-                        elseif ($alter == $alt2) { $alter = $alt1; }
-                    if (isset($avatar_usage[$file]) )
-                    {
-                        // Since we need to supply a link with a valid sid later in html, let's build it now
+                    # Alternating row colows code
+                    if($alter == $alt1): 
+					  $alter = $alt2; 
+                    elseif($alter == $alt2): 
+					  $alter = $alt1; 
+					endif;
+                    
+					if(isset($avatar_usage[$file])):
+                    
+                        # Since we need to supply a link with a valid sid later in html, let's build it now
                         $av_id = $avatar_usage[$file];
-                        $sql = "SELECT user_id FROM " . USERS_TABLE . "
-                            WHERE username = '$av_id'";
-                        if(!$result = $db->sql_query($sql))
-                        {
+                        $sql = "SELECT user_id FROM ".USERS_TABLE." WHERE username = ".$av_id;
+                        
+						if(!$result = $db->sql_query($sql)):
                             $error = $db->sql_error();
                             die("Could not get user information! $error[code] : $error[message]");
-                        }
-                        $av_uid = $db->sql_fetchrow($result);
+                        endif;
+                        
+						$av_uid = $db->sql_fetchrow($result);
                         $avatar_uid = $av_uid['user_id'];
                         $edit_url = append_sid("./admin_users.php?mode=edit&u=$avatar_uid");
-                        // Bingo, someone is using this avatar
-                        print "<tr><td class=avatar_listing bgcolor=$alter><img src=$real_avatar_dir/$file><br />$file</td>
+
+                        # Bingo, someone is using this avatar
+                        print "<tr><td class=avatar_listing bgcolor=$alter><img height=\"200px\" src=$real_avatar_dir/$file><br />$file</td>
                                <td class=avatar_listing bgcolor=$alter>$stats[7] Bytes</td>
                                <td class=avatar_listing bgcolor=$alter>$avatar_usage[$file]</td>
                                <td class=avatar_listing bgcolor=$alter>
                                                            <a href=\"$edit_url\"> Edit $avatar_usage[$file]</a></td></tr>\n";
 
-                    }
-                    else
-                    {
-                        // Not used, safe to display delete link for admin
+                    
+                    else:
+                        # Not used, safe to display delete link for admin
                         $delete_html = append_sid("./admin_avatar.php?mode=delete&target=$file");
-                        print "<tr><td class=avatar_listing bgcolor=$alter><img src=$real_avatar_dir/$file><br />$file</td>
+                        print "<tr><td class=avatar_listing bgcolor=$alter><img height=\"200px\" src=$real_avatar_dir/$file><br />$file</td>
                                                            <td class=avatar_listing bgcolor=$alter>$stats[7] Bytes</td>
                                <td class=avatar_listing bgcolor=$alter>Not Used<br /><a href=$delete_html onClick=\"if(confirm('Are you sure you want to delete: $file ?')) return true; else return false;\">Delete</a></td>
                                <td class=avatar_listing bgcolor=$alter>&nbsp;</td>
                                </tr>\n";
-                    }
-                }
-            }
-        }
-        else
-        {
-            // If we made it to this else there was a problem trying to read the avatar directory
-            // If you see this error message check this variable:
-            //     $real_avatar_dir -> This may be set incorrectly for your site.
+                    endif;
+                endif;
+				
+            endwhile;
+        else:
+            # If we made it to this else there was a problem trying to read the avatar directory
+            # If you see this error message check this variable:
+            # $real_avatar_dir -> This may be set incorrectly for your site.
             print "Avatar directory unavailable!";
-        }
+        endif;
 
         echo '</table>';
         break;
-}
+		
+endswitch;
 
 ?>
