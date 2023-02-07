2df296829fb34f4ff4d8183d20c7038f33b38037
Ernest Buffington <ernest.buffington@gmail.com>
2023-02-07 14:39:19 -0500

    Update css_toolbox.php

--- themes/BlackJack/css/css_toolbox.php
+++ themes/BlackJack/css/css_toolbox.php
@@ -105,6 +105,32 @@ define('BODY_CONTAIN', 'background-size: 100% auto, cover;
                              background-attachment: fixed;
                               background-repeat: repeat-y;'); // stretch this whore out
 ?>
+/*
+ * COMMENT SYSTEM IN BLOG ARTICLES
+ *----------------------------------------
+ */
+#respond {
+  margin-top: 40px;
+}
+
+#respond input[type='text'],
+#respond input[type='email'],
+#respond textarea {
+  margin-bottom: 10px;
+  display: block;
+  width: 100%;
+
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  -o-border-radius: 5px;
+  -ms-border-radius: 5px;
+  -khtml-border-radius: 5px;
+  border-radius: 5px;
+
+  line-height: 1.4em;
+}
+
 /**
  * Stylesheet for the Titanium Core Theme
  *
